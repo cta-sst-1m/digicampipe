ee27841d66098a818f5c271357f2315010c4795a
calispac <cyril.alispach@unige.ch>
2017-09-05 18:28:18 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='digicampipe',
+    version='0.1.0',
+    packages=['digicampipe'],
+    url='https://github.com/calispac/digicampipe',
+    license='GNU GPL 3.0',
+    author='Cyril Alispach',
+    author_email='cyril.alispach@gmail.com',
+    long_description=open('README.md').read(),
+    description='A package for DigiCam pipeline',
+    requires=['numpy', 'matplotlib', 'scipy', 'ctapipe'],
+)
