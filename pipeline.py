8bdc6fcf82236010b18853e05b4db82391571776
Victor Coco <victor.coco@cern.ch>
2017-09-15 17:54:26 +0200

    Victor

--- pipeline.py
+++ pipeline.py
@@ -8,9 +8,10 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Data configuration
-    directory = '/home/alispach/blackmonkey/calib_data/first_light/20170831/'
+    directory = '/data/datasets/CTA/REALDATA/'
     filename = directory + 'CameraDigicam@sst1mserver_0_000.%d.fits.fz'
-    file_list = [filename % number for number in range(10, 165)]
+    file_list = [filename % number for number in range(110,132)]
+    camera_config_file = '/data/software/CTS/config/camera_config.cfg'
 
     # Trigger configuration
     unwanted_patch = [391, 392, 403, 404, 405, 416, 417]
@@ -26,15 +27,17 @@ if __name__ == '__main__':
                                 'timing_width':6,
                                 'central_sample':11}
 
-    peak_position = fake_timing_hist(time_integration_options['n_samples'], time_integration_options['timing_width'],
+    peak_position = utils.fake_timing_hist(time_integration_options['n_samples'], time_integration_options['timing_width'],
                                      time_integration_options['central_sample'])
-    time_integration_options['peak'], time_integration_options['mask'], time_integration_options['mask_edges'] =\
-        generate_timing_mask(time_integration_options['window_start'],
+    time_integration_options['peak'], time_integration_options['mask'], time_integration_options['mask_edges'] = \
+        utils.generate_timing_mask(time_integration_options['window_start'],
                              time_integration_options['window_width'],
                              peak_position)
 
     # Create the calibration container
-    calib_data = initialise_calibration_data(n_samples_for_baseline = 10000)
+    calib_data = random_triggers.initialise_calibration_data(n_samples_for_baseline = 50000)
+
+    # Define the event stream
     # Get the actual data stream
     data_stream = event_stream(file_list=file_list, expert_mode=True)
     # Filter events
@@ -43,13 +46,11 @@ if __name__ == '__main__':
     data_stream = random_triggers.extract_baseline(data_stream,calib_data)
     # Run the r1 calibration
     data_stream = r1.calibrate_to_r1(data_stream,calib_data,time_integration_options)
+    # Run Hillas
+    #data_stream = dl2.calibrate_to_dl2(data_stream)
+    # Filter the proecssing level
 
-    n_events = 100000
-    n_pixels = 1296
+    data_stream = filter.filter_level(data_stream, level = 1)
 
-    time = np.zeros(n_events)
-
-    for i, event, calib in zip(range(n_events), data_stream):
-        camera_config_file = '/home/alispach/Documents/PhD/ctasoft/CTS/config/camera_config.cfg'
-        display = EventViewer(data_stream, camera_config_file=camera_config_file, scale='lin')
-        display.draw()
+    display = EventViewer(data_stream, camera_config_file=camera_config_file, scale='lin')
+    display.draw()
