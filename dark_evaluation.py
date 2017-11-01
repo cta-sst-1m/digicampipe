62b92259b647dbe644bd2e011d483c3b53006eb6
nagai <Andrii.Nagai@unige.ch>
2017-11-01 15:51:49 +0100

    Pxt vs Overvoltage and fNSB PDE vs Overvoltage, wavelength and fNSB Overvoltage drop vs. Overvoltage and fNSB

--- dark_evaluation.py
+++ dark_evaluation.py
@@ -11,13 +11,12 @@ import numpy as np
 if __name__ == '__main__':
     # Data configuration
 
-    directory = '/home/alispach/data/CRAB_01/'
+    directory = '/Users/nagai/ctasoft/data/CRAB_01/'
     filename = directory + 'CRAB_01_0_000.%03d.fits.fz'
-    file_list = [filename % number for number in [2]]
-    camera_config_file = '/home/alispach/ctasoft/CTS/config/camera_config.cfg'
+    file_list = [filename % number for number in [1]]
+    camera_config_file = '/Users/nagai/ctasoft/CTS/config/camera_config.cfg'
     dark_filename = 'dark.npz'
 
-    trigger_filename = 'bias_curve_dark.npz'
     thresholds = np.arange(0, 400, 10)
     unwanted_patch = [306, 318, 330, 342, 200]
     unwanted_cluster = [200]
@@ -27,26 +26,17 @@ if __name__ == '__main__':
     digicam_geometry = geometry.generate_geometry_from_camera(camera=digicam)
 
     # Define the event stream
-    data_stream = event_stream(file_list=file_list, expert_mode=True, camera_geometry=digicam_geometry)
+    data_stream = event_stream(file_list=file_list, camera=digicam, expert_mode=True, camera_geometry=digicam_geometry)
     data_stream = filter.set_patches_to_zero(data_stream, unwanted_patch=unwanted_patch)
     # Fill the flags (to be replaced by Digicam)
     data_stream = filter.filter_event_types(data_stream, flags=[8])
-
-    data_stream = save_bias_curve(data_stream,
-                                  thresholds=thresholds,
-                                  camera=digicam,
-                                  blinding=blinding,
-                                  output_filename=directory + trigger_filename,
-                                  unwanted_cluster=unwanted_cluster)
-
     data_stream = save_dark(data_stream, directory + dark_filename)
 
-    for i, data in enumerate(data_stream):
+    # for i, data in enumerate(data_stream):
 
-        print(i)
+    #    print(i)
 
     data_dark = np.load(directory + dark_filename)
-    data_rate = np.load(directory + trigger_filename)
 
     plt.figure()
     plt.hist(data_dark['baseline'], bins='auto')
@@ -58,13 +48,6 @@ if __name__ == '__main__':
     plt.xlabel('dark std [LSB]')
     plt.ylabel('count')
 
-    fig = plt.figure()
-    axis = fig.add_subplot(111)
-    axis.errorbar(data_rate['threshold'], data_rate['rate'] * 1E9, yerr=data_rate['rate_error'] * 1E9, label='Blinding : {}'.format(blinding))
-    axis.set_ylabel('rate [Hz]')
-    axis.set_xlabel('threshold [LSB]')
-    axis.set_yscale('log')
-    axis.legend(loc='best')
     plt.show()
 
 
