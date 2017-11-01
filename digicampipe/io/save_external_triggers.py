fixed border flag. moments calculation of histograms

digicampipe/io/save_external_triggers.py
@@ -2,7 +2,7 @@ import numpy as np
 import itertools
 
 
-def save_external_triggers(data_stream, output_filename, n_events=None, pixel_list=[...]):
+def save_external_triggers(data_stream, output_filename, n_events=None, pixel_list=[...], yielding=False):
 
     baseline = []
     baseline_dark = []
@@ -31,6 +31,8 @@ def save_external_triggers(data_stream, output_filename, n_events=None, pixel_li
             nsb_rate.append(list(event.r1.tel[telescope_id].nsb[pixel_list]))
             gain_drop.append(list(event.r1.tel[telescope_id].gain_drop[pixel_list]))
             time_stamp.append(event.r0.tel[telescope_id].local_camera_clock)
+        if yielding:
+            yield event
 
     np.savez(output_filename,
              baseline=np.array(baseline),

digicampipe/utils/utils.py
@@ -21,6 +21,16 @@ def compute_moments(bins, count):
     return moments
 
 
+def moving_average(data, n):
+
+    window = np.ones(n)
+    smoothed = ndimage.convolve1d(data, window, axis=-1, mode='reflect')
+    smoothed = smoothed / n
+    # mask = np.ones(data.shape[0]) * False
+    # mask[:smoothed.shape[0]] = True
+    return smoothed
+
+
 def integrate(data, window_width):
     """
     Simple integration function over N samples
@@ -105,7 +115,7 @@ def fake_timing_hist(n_samples,timing_width, central_sample):
     :return:
     """
     timing = np.zeros((1296,n_samples+1,),dtype=float)
-    timing[...,int(central_sample-timing_width):int(central_sample+timing_width)]=1.
+    timing[...,max(0, int(central_sample-timing_width)):min(int(central_sample+timing_width), n_samples+1)]=1.
     return timing
 
 

pipeline_crab.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
                                 'window_width': 7,
                                 'threshold_saturation': np.inf,
                                 'n_samples': 50,
-                                'timing_width': 6,
+                                'timing_width': 20, # 6 default
                                 'central_sample': 11}
 
     peak_position = utils.fake_timing_hist(time_integration_options['n_samples'],
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     shower_distance = 200 * u.mm
 
     # Filtering on big showers
-    min_photon = 20
+    min_photon = 10
 
     ####################
     ##### ANALYSIS #####
@@ -125,5 +125,5 @@ if __name__ == '__main__':
             pass
     else:
         # Save the hillas parameters
-        # save_hillas_parameters(data_stream=data_stream, n_showers=n_showers, output_filename=directory + hillas_filename)
-        save_hillas_parameters_in_text(data_stream=data_stream, output_filename=directory + hillas_filename)
+        save_hillas_parameters(data_stream=data_stream, n_showers=n_showers, output_filename=directory + hillas_filename)
+        # save_hillas_parameters_in_text(data_stream=data_stream, output_filename=directory + hillas_filename)

show_hillas.py
@@ -4,10 +4,11 @@ import plot_alpha_corrected
 import matplotlib.pyplot as plt
 
 directory = '/home/alispach/data/CRAB_01/'
-hillas_filename = directory + 'hillas.npz'
+hillas_filename = directory + 'hillas_test.npz'
 
 hillas = dict(np.load(hillas_filename))
 
+'''
 cut_size = 10000
 cut_width_length = 0.5
 cut_r = 350
@@ -38,11 +39,88 @@ for source_x in source_xs:
 print(true_source, alpha_max)
 
 # hillas_corr = plot_alpha_corrected.correct_alpha(hillas, source_x=true_source[0], source_y=true_source[1])
-plot.plot_hillas(hillas_dict=hillas, bins='auto')#, title='Crab (%0.1f, %0.1f)' %(true_source[0], true_source[1]))
+# plot.plot_hillas(hillas_dict=hillas, bins='auto')#, title='Crab (%0.1f, %0.1f)' %(true_source[0], true_source[1]))
+'''
+mask = (np.ones(hillas['size'].shape[0]) > 0)
+
+for key, val in hillas.items():
+
+    mask *= np.isfinite(val)
+
+for key, val in hillas.items():
+    hillas[key] = val[mask]
+
 
 hillas['time_spread'] = hillas['time_spread'][np.isfinite(hillas['time_spread'])]
 
 plt.figure()
 plt.hist(hillas['time_spread'], bins='auto')
 plt.xlabel('time spread [ns]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['cen_x'], bins='auto')
+plt.xlabel('$x$ [mm]')
+plt.ylabel('count')
+
+
+plt.figure()
+plt.hist(hillas['cen_y'], bins='auto')
+plt.xlabel('$y$ [mm]')
+plt.ylabel('count')
+
+
+plt.figure()
+plt.hist(hillas['length'], bins='auto')
+plt.xlabel('$l$ [mm]')
+plt.ylabel('count')
+
+
+plt.figure()
+plt.hist(hillas['width'], bins='auto')
+plt.xlabel('$w$ [mm]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['phi'], bins='auto')
+plt.xlabel('$\phi$ [rad]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['psi'], bins='auto')
+plt.xlabel('$\psi$ [rad]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['miss'], bins='auto')
+plt.xlabel('miss [mm]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['skewness'], bins='auto' , label='skewness []', alpha=0.7)
+plt.hist(hillas['kurtosis'], bins='auto', label='kurtosis []', alpha=0.7)
+plt.legend()
+plt.xlabel('[]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['r'], bins='auto')
+plt.xlabel('r [mm]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(np.arcsin(hillas['miss']/hillas['r']), bins='auto')
+plt.xlabel(r'$\alpha$ [rad]')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['width']/hillas['length'], bins='auto')
+plt.xlabel(r'$\frac{w}{l}$ []')
+plt.ylabel('count')
+
+plt.figure()
+plt.hist(hillas['size'], bins='auto', log=True)
+plt.xlabel('size [a.u.]')
+plt.ylabel('count')
+
 plt.show()