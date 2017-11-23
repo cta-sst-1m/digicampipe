fixed border flag. moments calculation of histograms

bias_curve_mc.py
@@ -9,9 +9,9 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
-    directory = '/home/alispach/data/digicam_commissioning/trigger/mc/'
-    filename = directory + 'digicam_toy_2.hdf5'
-    file_list = [filename]
+    directory = '/home/alispach/data/digicamtoy/'
+    filename = 'nsb_0.hdf5'
+    file_list = [directory + filename]
     digicam_config_file = '/home/alispach/ctasoft/CTS/config/camera_config.cfg'
 
     digicam = Camera(_config_file=digicam_config_file)
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     thresholds = np.arange(0, 400, 10)
     blinding = True
-    trigger_filename = 'test.npz'
+    trigger_filename = filename.strip('.hdf5') + '_bias_curve.npz'
 
     ####################
 
@@ -32,31 +32,40 @@ if __name__ == '__main__':
     data_stream = filter.filter_missing_baseline(data_stream)
     data_stream = r0.fill_trigger_patch(data_stream)
     data_stream = r0.fill_trigger_input_7(data_stream)
-    data_stream = r0.fill_trigger_input_19(data_stream)
+    # data_stream = r0.fill_trigger_input_19(data_stream)
     data_stream = save_bias_curve(data_stream, thresholds=thresholds, blinding=blinding, output_filename=directory + trigger_filename)
 
-    #for i, data in enumerate(data_stream):
+    # for i, data in enumerate(data_stream):
 
-    #    print(i)
+      #  print(i)
 
-    trigger_mc = np.load(directory + 'trigger.npz')
-    trigger_mc_1 = np.load(directory + 'test.npz')
-    trigger_mc_2 = np.load(directory + 'trigger_non_uniform_nsb.npz')
-    trigger = np.load('/home/alispach/data/CRAB_01/' + 'trigger.npz')
+    # trigger_mc = np.load(directory + 'trigger.npz')
+    # trigger_mc_1 = np.load(directory + 'test.npz')
+    # trigger_mc_2 = np.load(directory + 'trigger_non_uniform_nsb.npz')
+    # trigger = np.load('/home/alispach/data/CRAB_01/' + 'trigger.npz')
+    trigger_dark = np.load(directory + trigger_filename)
 
     fig = plt.figure()
     axis = fig.add_subplot(111)
-    axis.errorbar(trigger_mc['threshold'], trigger_mc['rate'] * 1E9, yerr=trigger_mc['rate_error'] * 1E9,
-                  label='DigicamToy')
-    axis.errorbar(trigger_mc_1['threshold'], trigger_mc_1['rate'] * 1E9, yerr=trigger_mc_1['rate_error'] * 1E9,
-                  label='DigicamToy New')
-    axis.errorbar(trigger['threshold'], trigger['rate'] * 1E9, yerr=trigger['rate_error'] * 1E9,
-                  label='Data')
-    axis.errorbar(trigger_mc_2['threshold'], trigger_mc_2['rate'] * 1E9, yerr=trigger_mc_2['rate_error'] * 1E9,
-                  label='DigicamToy NSB 1.2 +- 0.19 [GHz]')
+
+    care_threshold = [100.,  120.,  140.,  160.,  180.,  200.]
+    care_rate = [4.99999976e+06,   4.99999976e+06,   4.99999976e+06,   4.99843976e+06,  1.73769992e+05,   1.19999994e+02]
+    care_err = [150755.66512837,  150755.66512837,  150755.66512837,  150732.14540993,  28104.5114053,   738.5489108 ]
+    # axis.errorbar(trigger_mc['threshold'], trigger_mc['rate'] * 1E9, yerr=trigger_mc['rate_error'] * 1E9,
+    #              label='DigicamToy $f_{nsb} = 1.2$ [GHz]')
+    # axis.errorbar(trigger_mc_1['threshold'], trigger_mc_1['rate'] * 1E9, yerr=trigger_mc_1['rate_error'] * 1E9,
+    #              label='DigicamToy New')
+    # axis.errorbar(trigger['threshold'], trigger['rate'] * 1E9, yerr=trigger['rate_error'] * 1E9,
+    #               label='Data', linestyle='--', color='k')
+    # axis.errorbar(care_threshold, care_rate, yerr=care_err, label='CARE $f_{nsb} = 1.2$ [GHz]')
+    # axis.errorbar(trigger_mc_2['threshold'], trigger_mc_2['rate'] * 1E9, yerr=trigger_mc_2['rate_error'] * 1E9,
+    #              label='DigicamToy $f_{nsb} = 1.2 \pm 0.19$ [GHz]')
+    axis.errorbar(trigger_dark['threshold'], trigger_dark['rate'] * 1E9, yerr=trigger_dark['rate_error'] * 1E9,
+                  label='DigicamToy $f_{nsb}$ = 3 [MHz]')
     axis.set_ylabel('rate [Hz]')
     axis.set_xlabel('threshold [LSB]')
     axis.set_yscale('log')
     axis.legend(loc='best')
     # fig.savefig(directory + 'bias_curve.svg')
+
     plt.show()

dark_evaluation.py
@@ -1,4 +1,4 @@
-from digicampipe.calib.camera import filter
+from digicampipe.calib.camera import filter, r0
 from digicampipe.io.event_stream import event_stream
 from digicampipe.io.save_adc import save_dark
 from digicampipe.io.save_bias_curve import save_bias_curve
@@ -6,20 +6,21 @@ from cts_core.camera import Camera
 from digicampipe.utils import geometry
 import matplotlib.pyplot as plt
 import numpy as np
-
+from digicamviewer.viewer import EventViewer
 
 if __name__ == '__main__':
     # Data configuration
 
     directory = '/home/alispach/data/CRAB_01/'
     filename = directory + 'CRAB_01_0_000.%03d.fits.fz'
-    file_list = [filename % number for number in [2]]
+    file_list = [filename % number for number in [3, ]]
     camera_config_file = '/home/alispach/ctasoft/CTS/config/camera_config.cfg'
     dark_filename = 'dark.npz'
 
     trigger_filename = 'bias_curve_dark.npz'
     thresholds = np.arange(0, 400, 10)
     unwanted_patch = [306, 318, 330, 342, 200]
+    pixel_not_wanted = [1038, 1039, 1002, 1003, 1004, 966, 967, 968, 930, 931, 932, 896]
     unwanted_cluster = [200]
     blinding = True
 
@@ -28,21 +29,30 @@ if __name__ == '__main__':
 
     # Define the event stream
     data_stream = event_stream(file_list=file_list, expert_mode=True, camera_geometry=digicam_geometry, camera=digicam)
-    data_stream = filter.set_patches_to_zero(data_stream, unwanted_patch=unwanted_patch)
+    data_stream = filter.set_pixels_to_zero(data_stream, unwanted_pixels=pixel_not_wanted)
+    # data_stream = filter.set_patches_to_zero(data_stream, unwanted_patch=unwanted_patch)
     # Fill the flags (to be replaced by Digicam)
     data_stream = filter.filter_event_types(data_stream, flags=[8])
 
-    # data_stream = save_bias_curve(data_stream,
-    #                              thresholds=thresholds,
-    #                              blinding=blinding,
-    #                              output_filename=directory + trigger_filename,
-    #                              unwanted_cluster=unwanted_cluster)
+    data_stream = r0.fill_trigger_input_7(data_stream)
+    data_stream = save_bias_curve(data_stream,
+                                  thresholds=thresholds,
+                                  blinding=blinding,
+                                  output_filename=directory + trigger_filename,
+                                  unwanted_cluster=unwanted_cluster)
 
     data_stream = save_dark(data_stream, directory + dark_filename)
 
     for i, data in enumerate(data_stream):
 
         print(i)
+    """
+    with plt.style.context('ggplot'):
+        display = EventViewer(data_stream, n_samples=50, camera_config_file=camera_config_file,
+                              scale='lin')  # , limits_colormap=[10, 500])
+        display.draw()
+        pass
+    """
 
     data_dark = np.load(directory + dark_filename)
     data_rate = np.load(directory + trigger_filename)
@@ -56,6 +66,7 @@ if __name__ == '__main__':
     plt.hist(data_dark['standard_deviation'], bins='auto')
     plt.xlabel('dark std [LSB]')
     plt.ylabel('count')
+
 
     fig = plt.figure()
     axis = fig.add_subplot(111)

show_hillas.py
@@ -3,124 +3,105 @@ from digicampipe.visualization import plot
 import plot_alpha_corrected
 import matplotlib.pyplot as plt
 
-directory = '/home/alispach/data/CRAB_01/'
-hillas_filename = directory + 'hillas_test.npz'
 
-hillas = dict(np.load(hillas_filename))
+def correct_hillas(data, source_x, source_y):
 
-'''
-cut_size = 10000
-cut_width_length = 0.5
-cut_r = 350
+    data['cen_x'] = data['cen_x'] - source_x
+    data['cen_y'] = data['cen_y'] - source_y
+    data['r'] = np.sqrt((data['cen_x'])**2 + (data['cen_y'])**2)
+    data['phi'] = np.arctan2(data['cen_y'], data['cen_x'])
 
-source_xs = [0] # [-50, -40, -30, -20, -10, 0, 10, 20, 30, 40, 50]
-source_ys = source_xs
+    data['alpha'] = np.cos(data['phi']-data['psi'])
+    data['alpha'] = np.arccos(data['alpha'])
 
-alpha_max = 0
+    mask = data['alpha'] > (np.pi / 2)
+    data['alpha'][mask] = np.pi - data['alpha'][mask]
+    data['alpha'] = np.rad2deg(data['alpha'])
+    data['phi'] = np.rad2deg(data['phi'])
+    data['psi'] = np.rad2deg(data['psi'])
+    # data['alpha'] = np.abs(data['phi'] - data['psi'])
+    # data['alpha'] = np.remainder(data['alpha'], np.pi/2)
+    data['miss'] = data['r'] * np.sin(data['alpha'])
 
-for source_x in source_xs:
-    for source_y in source_ys:
-
-        hillas_cor = plot_alpha_corrected.correct_alpha(hillas, source_x=source_x, source_y=source_y)
-
-        alpha_histo = np.histogram(hillas_cor['alpha'], bins=30)
-
-        alpha_0_count = alpha_histo[0][0]
-        print(alpha_0_count)
-
-        if alpha_0_count >= alpha_max:
-
-            print(alpha_max)
-            alpha_max = alpha_0_count
-            true_source = [source_x, source_y]
-            # hillas = hillas_cor
+    return data
 
 
-print(true_source, alpha_max)
+def plot_hillas_parameter(parameter, name, units='', axis=None, bins='auto', log_x=False, log_y=False, legend=True, errorbar=False, **kwargs):
 
-# hillas_corr = plot_alpha_corrected.correct_alpha(hillas, source_x=true_source[0], source_y=true_source[1])
-# plot.plot_hillas(hillas_dict=hillas, bins='auto')#, title='Crab (%0.1f, %0.1f)' %(true_source[0], true_source[1]))
-'''
-mask = (np.ones(hillas['size'].shape[0]) > 0)
+    if axis is None:
 
-for key, val in hillas.items():
+        fig = plt.figure()
+        axis = fig.add_subplot(111)
 
-    mask *= np.isfinite(val)
+    hist, bins = np.histogram(parameter, bins=bins)
+    axis.step(bins[:-1], hist, where='mid', label=name, **kwargs)
+    if errorbar:
+        axis.errorbar(bins[:-1], hist, yerr=np.sqrt(hist), fmt='k', linestyle='None', **kwargs)
+    # axis.set_xlabel(name + units)
+    if log_x:
+        axis.set_xscale('log')
+    if log_y:
+        axis.set_yscale('log')
+    # axis.set_ylabel('count')
+    if legend:
+        axis.legend(loc='best')
 
-for key, val in hillas.items():
-    hillas[key] = val[mask]
-
-
-hillas['time_spread'] = hillas['time_spread'][np.isfinite(hillas['time_spread'])]
-
-plt.figure()
-plt.hist(hillas['time_spread'], bins='auto')
-plt.xlabel('time spread [ns]')
-plt.ylabel('count')
-
-plt.figure()
-plt.hist(hillas['cen_x'], bins='auto')
-plt.xlabel('$x$ [mm]')
-plt.ylabel('count')
+    return axis
 
 
-plt.figure()
-plt.hist(hillas['cen_y'], bins='auto')
-plt.xlabel('$y$ [mm]')
-plt.ylabel('count')
+if __name__ == '__main__':
 
 
-plt.figure()
-plt.hist(hillas['length'], bins='auto')
-plt.xlabel('$l$ [mm]')
-plt.ylabel('count')
+    """
+    directory = '/home/alispach/data/CRAB_01/'
+    hillas_filename = directory + 'hillas_crab_all_cut_100pe.npz'
+    hillas = dict(np.load(hillas_filename))
+    """
 
+    directory = '/home/alispach/data/CRAB_02/'
+    hillas_filename = directory + 'crab_2nd.txt'
+    hillas = np.loadtxt(hillas_filename)
+    hillas = hillas.T
+    names = ["size", "cen_x", "cen_y", "length", "width", "r", "phi", "psi", "miss", "skewness", "kurtosis", "event_number", "time_stamp", 'border']
+    hillas = dict(zip(names, hillas))
 
-plt.figure()
-plt.hist(hillas['width'], bins='auto')
-plt.xlabel('$w$ [mm]')
-plt.ylabel('count')
+    x, y = 40, 13.5
+    hillas = correct_hillas(hillas, source_x=x, source_y=y)
 
-plt.figure()
-plt.hist(hillas['phi'], bins='auto')
-plt.xlabel('$\phi$ [rad]')
-plt.ylabel('count')
+    mask = (np.ones(hillas['size'].shape[0]) > 0)
+    print(np.sum(mask))
+    mask *= hillas['width']/hillas['length'] < 2/3
+    # mask *= hillas['alpha'] < 4
+    try:
+        mask *= hillas['border'] == 0
+    except:
+        print('no border parameter')
 
-plt.figure()
-plt.hist(hillas['psi'], bins='auto')
-plt.xlabel('$\psi$ [rad]')
-plt.ylabel('count')
+    for key, val in hillas.items():
 
-plt.figure()
-plt.hist(hillas['miss'], bins='auto')
-plt.xlabel('miss [mm]')
-plt.ylabel('count')
+        mask *= np.isfinite(val)
 
-plt.figure()
-plt.hist(hillas['skewness'], bins='auto' , label='skewness []', alpha=0.7)
-plt.hist(hillas['kurtosis'], bins='auto', label='kurtosis []', alpha=0.7)
-plt.legend()
-plt.xlabel('[]')
-plt.ylabel('count')
+    for key, val in hillas.items():
+        hillas[key] = val[mask]
 
-plt.figure()
-plt.hist(hillas['r'], bins='auto')
-plt.xlabel('r [mm]')
-plt.ylabel('count')
+    n_bins = 40
 
-plt.figure()
-plt.hist(np.arcsin(hillas['miss']/hillas['r']), bins='auto')
-plt.xlabel(r'$\alpha$ [rad]')
-plt.ylabel('count')
+    title = 'Hillas parameters'
+    figure, axis_array = plt.subplots(3, 4)
+    figure.subplots_adjust(top=0.95)
 
-plt.figure()
-plt.hist(hillas['width']/hillas['length'], bins='auto')
-plt.xlabel(r'$\frac{w}{l}$ []')
-plt.ylabel('count')
+    plot_hillas_parameter(hillas['alpha'], r'$\alpha$ [deg]', axis=axis_array[2, 2], bins=np.arange(0, 90 + 4, 4), errorbar=True)
+    plot_hillas_parameter(hillas['cen_x'], '$x$ [mm]', axis=axis_array[0, 0], bins=n_bins)
+    plot_hillas_parameter(hillas['cen_y'], '$y$ [mm]', axis=axis_array[0, 1], bins=n_bins)
+    plot_hillas_parameter(hillas['length'], 'length [mm]', axis=axis_array[1, 0], bins=n_bins)
+    plot_hillas_parameter(hillas['width'], 'width [mm]', axis=axis_array[1, 1], bins=n_bins)
+    plot_hillas_parameter(hillas['phi'], '$\phi$ [deg]', axis=axis_array[2, 0], bins=n_bins//2)
+    plot_hillas_parameter(hillas['psi'], '$\psi$ [deg]', axis=axis_array[2, 1], bins=n_bins//2)
+    plot_hillas_parameter(hillas['miss'], 'miss [mm]', axis=axis_array[0, 3], bins=n_bins)
+    plot_hillas_parameter(hillas['skewness'], 'skewness', axis=axis_array[2, 3], legend=True, bins=n_bins)
+    plot_hillas_parameter(hillas['kurtosis'], 'kurtosis', axis=axis_array[2, 3], legend=True, bins=n_bins)
+    plot_hillas_parameter(hillas['r'], 'r [mm]', axis=axis_array[0, 2], bins=n_bins)
+    plot_hillas_parameter(hillas['size'], 'size [a.u.]', axis=axis_array[1, 3], bins=n_bins, log_y=True, log_x=True)
+    plot_hillas_parameter(hillas['width']/hillas['length'], 'width/length []', axis=axis_array[1, 2], bins=n_bins)
 
-plt.figure()
-plt.hist(hillas['size'], bins='auto', log=True)
-plt.xlabel('size [a.u.]')
-plt.ylabel('count')
-
-plt.show()
+    plt.show()