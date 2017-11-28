0137267e4dee0739792a7668bcbe3661c909ec4c
Dominik Neise <neised@phys.ethz.ch>
2017-11-28 09:26:33 +0100

    use `with` and `tqdm` over open-close and print statements

--- digicampipe/io/save_hillas.py
+++ digicampipe/io/save_hillas.py
@@ -43,31 +43,27 @@ def save_hillas_parameters(data_stream, output_filename):
 
 def save_hillas_parameters_in_text(data_stream, output_filename):
 
+    with open(output_filename, 'w') as ofile:
 
-    print("Opening output file" + output_filename)
-    ofile = open(output_filename, 'w')
-    ofile.write("# size cen_x cen_y length width r phi psi miss skewness kurtosis event_number timestamp border time spread\n")
-    for event in data_stream:
+        ofile.write("# size cen_x cen_y length width r phi psi miss skewness kurtosis event_number timestamp border time spread\n")
+        for event in tqdm(data_stream):
 
-        size         = event.dl2.shower.size
-        cen_x        = event.dl2.shower.cen_x.value
-        cen_y        = event.dl2.shower.cen_y.value
-        length       = event.dl2.shower.length.value
-        width        = event.dl2.shower.width.value
-        r            = event.dl2.shower.r.value
-        phi          = event.dl2.shower.phi.value
-        psi          = event.dl2.shower.psi.value
-        miss         = event.dl2.shower.miss.value
-        skewness     = event.dl2.shower.skewness
-        kurtosis     = event.dl2.shower.kurtosis
-        event_number = event.r0.event_id
-        border       = 1 if event.dl1.tel[1].on_border else 0
-        time_spread  = event.dl1.tel[1].time_spread
-        time_stamp = event.r0.tel[1].local_camera_clock
+            size         = event.dl2.shower.size
+            cen_x        = event.dl2.shower.cen_x.value
+            cen_y        = event.dl2.shower.cen_y.value
+            length       = event.dl2.shower.length.value
+            width        = event.dl2.shower.width.value
+            r            = event.dl2.shower.r.value
+            phi          = event.dl2.shower.phi.value
+            psi          = event.dl2.shower.psi.value
+            miss         = event.dl2.shower.miss.value
+            skewness     = event.dl2.shower.skewness
+            kurtosis     = event.dl2.shower.kurtosis
+            event_number = event.r0.event_id
+            border       = 1 if event.dl1.tel[1].on_border else 0
+            time_spread  = event.dl1.tel[1].time_spread
+            time_stamp = event.r0.tel[1].local_camera_clock
 
-        ofile.write(str(size) + " " + str(cen_x) + " " + str(cen_y) + " " + str(length) + " " + str(width) + " " + str(r) + " "+ str(phi) + " "+ str(psi) + " "+ str(miss) + " "+ str(skewness) + " "+ str(kurtosis) + " "+ str(event_number) + " "+ str(time_stamp) + " " + str(border) + " " + str(time_spread) + "\n")
-        ofile.flush()
-
-    ofile.close()
-    print("All done !")
+            ofile.write(str(size) + " " + str(cen_x) + " " + str(cen_y) + " " + str(length) + " " + str(width) + " " + str(r) + " "+ str(phi) + " "+ str(psi) + " "+ str(miss) + " "+ str(skewness) + " "+ str(kurtosis) + " "+ str(event_number) + " "+ str(time_stamp) + " " + str(border) + " " + str(time_spread) + "\n")
+            ofile.flush()
 
