6514c2bbcf7f2fd209f880520e2cbd3b7c083b69
Dominik Neise <neised@phys.ethz.ch>
2017-11-28 09:23:41 +0100

    avoid repeating myself in the np.savez call, so I cannot forget one element

--- digicampipe/io/save_hillas.py
+++ digicampipe/io/save_hillas.py
@@ -4,41 +4,41 @@ from tqdm import tqdm
 
 def save_hillas_parameters(data_stream, n_showers, output_filename):
 
-    size = []
-    cen_x = []
-    cen_y = []
-    length = []
-    width = []
-    r = []
-    phi = []
-    psi = []
-    miss = []
-    skewness = []
-    kurtosis = []
-    event_number = []
-    time_stamp = []
-    time_spread = []
+    output = {
+        'size': [],
+        'cen_x': [],
+        'cen_y': [],
+        'length': [],
+        'width': [],
+        'r': [],
+        'phi': [],
+        'psi': [],
+        'miss': [],
+        'skewness': [],
+        'kurtosis': [],
+        'event_number': [],
+        'time_stamp': [],
+        'time_spread': [],
+    }
 
     for i, event in enumerate(tqdm(data_stream)):
 
-        size.append(event.dl2.shower.size)
-        cen_x.append(event.dl2.shower.cen_x.value)
-        cen_y.append(event.dl2.shower.cen_y.value)
-        length.append(event.dl2.shower.length.value)
-        width.append(event.dl2.shower.width.value)
-        r.append(event.dl2.shower.r.value)
-        phi.append(event.dl2.shower.phi.value)
-        psi.append(event.dl2.shower.psi.value)
-        miss.append(event.dl2.shower.miss.value)
-        skewness.append(event.dl2.shower.skewness)
-        kurtosis.append(event.dl2.shower.kurtosis)
-        event_number.append(event.r0.event_id)
-        time_spread.append(event.dl1.tel[1].time_spread)
-        time_stamp.append(event.r0.tel[1].local_camera_clock)
+        output['size'].append(event.dl2.shower.size)
+        output['cen_x'].append(event.dl2.shower.cen_x.value)
+        output['cen_y'].append(event.dl2.shower.cen_y.value)
+        output['length'].append(event.dl2.shower.length.value)
+        output['width'].append(event.dl2.shower.width.value)
+        output['r'].append(event.dl2.shower.r.value)
+        output['phi'].append(event.dl2.shower.phi.value)
+        output['psi'].append(event.dl2.shower.psi.value)
+        output['miss'].append(event.dl2.shower.miss.value)
+        output['skewness'].append(event.dl2.shower.skewness)
+        output['kurtosis'].append(event.dl2.shower.kurtosis)
+        output['event_number'].append(event.r0.event_id)
+        output['time_spread'].append(event.dl1.tel[1].time_spread)
+        output['time_stamp'].append(event.r0.tel[1].local_camera_clock)
 
-    np.savez(output_filename, size=size, cen_x=cen_x, cen_y=cen_y, length=length, width=width, r=r, phi=phi, psi=psi,
-             miss=miss, skewness=skewness, kurtosis=kurtosis, event_number=event_number, time_stamp=time_stamp,
-             time_spread=time_spread)
+    np.savez(output_filename, **output)
 
 
 def save_hillas_parameters_in_text(data_stream, output_filename):
