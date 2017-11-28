43961ce23a7ce824caf2d5705a0f5c97e5527cb2
Dominik Neise <neised@phys.ethz.ch>
2017-11-28 10:25:13 +0100

    rename i --> event_id  scope was too large for 1-letter name

--- digicampipe/io/save_bias_curve.py
+++ digicampipe/io/save_bias_curve.py
@@ -12,15 +12,15 @@ def save_bias_curve(
 ):
     rate = np.zeros(thresholds.shape)
 
-    for i, event in enumerate(data_stream):
-        if i > n_events:
+    for event_id, event in enumerate(data_stream):
+        if event_id >= n_events:
             break
 
         for telescope_id in event.r0.tels_with_data:
 
             trigger_input_7 = event.r0.tel[telescope_id].trigger_input_7
 
-            if i == 0:
+            if event_id == 0:
 
                 n_clusters = trigger_input_7.shape[0]
                 cluster_rate = np.zeros((n_clusters, thresholds.shape[-1]))
@@ -29,19 +29,17 @@ def save_bias_curve(
 
                 comp = trigger_input_7 > threshold
 
-                if blinding:
+                if blinding and np.any(comp):
 
-                    if np.any(comp):
+                    if by_cluster:
+                        index = np.where(comp)[0]
+                        cluster_rate[index, thresholds.shape[0] - j - 1] += 1
+                        rate[thresholds.shape[0] - j - 1] += 1
 
-                        if by_cluster:
-                            index = np.where(comp)[0]
-                            cluster_rate[index, thresholds.shape[0] - j - 1] += 1
-                            rate[thresholds.shape[0] - j - 1] += 1
+                    else:
 
-                        else:
-
-                            rate[0:thresholds.shape[0] - j] += 1
-                            break
+                        rate[0:thresholds.shape[0] - j] += 1
+                        break
 
                 else:
                     comp = np.sum(comp, axis=0)
@@ -57,12 +55,17 @@ def save_bias_curve(
 
         yield event
 
-    time = ((i + 1) * 4. * trigger_input_7.shape[-1])
+    time = ((event_id + 1) * 4. * trigger_input_7.shape[-1])
     rate_error = np.sqrt(rate) / time
     cluster_rate_error = np.sqrt(cluster_rate) / time
     rate = rate / time
     cluster_rate = cluster_rate / time
 
-    np.savez(output_filename, rate=rate, rate_error=rate_error, cluster_rate=cluster_rate, cluster_rate_error=cluster_rate_error, threshold=thresholds)
-
-
+    np.savez(
+        output_filename,
+        rate=rate,
+        rate_error=rate_error,
+        cluster_rate=cluster_rate,
+        cluster_rate_error=cluster_rate_error,
+        threshold=thresholds
+    )
