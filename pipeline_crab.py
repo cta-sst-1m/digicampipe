fixed border flag. moments calculation of histograms

pipeline_crab.py
@@ -13,7 +13,6 @@ from optparse import OptionParser
 
 if __name__ == '__main__':
 
-
     parser = OptionParser()
     parser.add_option("-p", "--path", dest="directory", help="directory to data files",
                       default='/sst1m/raw/2017/09/28/CRAB_01/')
@@ -55,7 +54,7 @@ if __name__ == '__main__':
     unwanted_patch = None  # [306, 318, 330, 342] #[391, 392, 403, 404, 405, 416, 417]
 
     # Noisy pixels not taken into account in Hillas
-    pixel_not_wanted = [1038, 1039, 1002, 1003, 1004, 966, 967, 968, 930, 931, 932, 896]
+    pixel_not_wanted = [1038, 1039, 1002, 1003, 1004, 966, 967, 968, 930, 931, 932, 896] # Pixels of noisy module 96
     additional_mask = np.ones(1296)
     additional_mask[pixel_not_wanted] = 0
     additional_mask = additional_mask > 0