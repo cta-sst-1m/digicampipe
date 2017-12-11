c4b483490951cb7b166d678be957e78a7fb57310
Dominik Neise <neised@phys.ethz.ch>
2017-12-11 16:26:53 +0100

    move rawer tests up

--- digicampipe/tests/test_read_raw_events.py
+++ digicampipe/tests/test_read_raw_events.py
@@ -13,18 +13,6 @@ example_file_path = pkg_resources.resource_filename(
     )
 )
 
-
-def test_zfile_raises_on_wrong_path():
-    from digicampipe.io.protozfitsreader import ZFile
-    with pytest.raises(FileNotFoundError):
-        ZFile('foo.bar')
-
-
-def test_zfile_opens_correct_path():
-    from digicampipe.io.protozfitsreader import ZFile
-    ZFile(example_file_path)
-
-
 def test_rawreader_can_work_with_relative_path():
     from protozfitsreader import rawzfitsreader
     from protozfitsreader import L0_pb2
@@ -48,6 +36,17 @@ def test_rawreader_can_work_with_absolute_path():
 
     event = L0_pb2.CameraEvent()
     event.ParseFromString(raw)
+
+
+def test_zfile_raises_on_wrong_path():
+    from digicampipe.io.protozfitsreader import ZFile
+    with pytest.raises(FileNotFoundError):
+        ZFile('foo.bar')
+
+
+def test_zfile_opens_correct_path():
+    from digicampipe.io.protozfitsreader import ZFile
+    ZFile(example_file_path)
 
 
 def test_can_iterate_over_events():
