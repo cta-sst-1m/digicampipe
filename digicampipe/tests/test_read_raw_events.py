24c9f5dcb80641faa42568ffe9570f7735aa4ea7
Dominik Neise <neised@phys.ethz.ch>
2017-12-11 16:07:47 +0100

    add a test, to show all fields exist

--- digicampipe/tests/test_read_raw_events.py
+++ digicampipe/tests/test_read_raw_events.py
@@ -58,3 +58,30 @@ def test_can_iterate_over_events():
     for __ in zfits.move_to_next_event():
         pass
 
+
+def test_iteration_yield_expected_fields():
+    from digicampipe.io.protozfitsreader import ZFile
+
+    zfits = ZFile(example_file_path)
+
+    for __ in zfits.move_to_next_event():
+        # we just want to see, that the zfits file has all these
+        # fields and we can access them
+        zfits.get_event_number()
+        zfits.event.telescopeID
+        zfits.event.num_gains
+        zfits.get_number_of_pixels()
+        zfits.event.eventNumber
+        zfits.get_pixel_flags()
+        zfits.get_local_time()
+        zfits.get_central_event_gps_time()
+        zfits.get_camera_event_type()
+        zfits.get_array_event_type()
+        zfits.get_num_samples()
+        zfits.get_adcs_samples()
+
+        # expert mode fields
+        zfits.get_trigger_input_traces()
+        zfits.get_trigger_output_patch7()
+        zfits.get_trigger_output_patch19()
+        zfits.get_baseline()
