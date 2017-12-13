f64615a496c78e306abbe278817a2cb7bed04399
Dominik Neise <neised@phys.ethz.ch>
2017-12-13 21:16:49 +0100

    make test run

--- digicampipe/tests/test_lid_ccd.py
+++ digicampipe/tests/test_lid_ccd.py
@@ -1,6 +1,13 @@
 from digicampipe.image.sky_image import *
 import os
 
+from pkg_resources import resource_filename
+from glob import glob
+
+example_lid_CCD_image_file_paths = glob(
+    resource_filename('digicampipe', 'tests/resources/stars_on_lid/*.fits')
+)
+
 
 def test_find_stars():
     # find stars in lid CCD images:
@@ -14,13 +21,12 @@ def test_find_stars():
         (770, 1550),
         # (1000, 2050),
         (1300, 2400), ]
-    images_directory = './resources/stars_on_lid'
-    image_files = []
-    for file in os.listdir(images_directory):
-        if file.endswith(".fits"):
-            image_files.append(os.path.join(images_directory, file))
-    lidccd_obs = LidCCDObservation(image_files, crop_pixels1, crop_pixels2,
-                                   scale_low_images_deg=8., scale_high_images_deg=12.)
+    lidccd_obs = LidCCDObservation(
+        example_lid_CCD_image_file_paths,
+        crop_pixels1,
+        crop_pixels2,
+        scale_low_images_deg=8.,
+        scale_high_images_deg=12.)
     nsolved = 0
     for lidccd_image in lidccd_obs.lidccd_images:
         if lidccd_image.wcs is not None:
