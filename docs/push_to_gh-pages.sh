48abdd22484eed1bba03c58deea67f36bcbbbdb4
calispac <cyril.alispach@unige.ch>
2017-12-20 21:23:13 +0100

    removed make.bat

--- docs/push_to_gh-pages.sh
+++ docs/push_to_gh-pages.sh
@@ -5,13 +5,13 @@
 ORG=calispac
 REPO=digicampipe
 # This probably should match an email for one of your users.
-EMAIL=example@example.com
+EMAIL=Travis
 
 set -e
 
 # Clone the gh-pages branch outside of the repo and cd into it.
 cd ..
-git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
+git clone -b gh-pages "https://$GITHUB_API_KEY@github.com/$ORG/$REPO.git" gh-pages
 cd gh-pages
 
 # Update git configuration so I can push.
@@ -29,5 +29,5 @@ git add -A .
 git commit -m "[ci skip] Autodoc commit for $COMMIT."
 if [ "$1" != "dry" ]; then
     # -q is very important, otherwise you leak your GH_TOKEN
-    git push -q origin gh-pages
+    git push --quiet --force https://$ORG:$GITHUB_API_KEY@github.com/$ORG/$REPO-gh-pages gh-pages
 fi
