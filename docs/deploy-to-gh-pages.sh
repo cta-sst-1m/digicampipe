60aaf252e9e7501c6250d293c3278d66ad45f5b0
calispac <cyril.alispach@unige.ch>
2017-12-20 21:57:32 +0100

    test gh-pages

---
+++ docs/deploy-to-gh-pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# See https://medium.com/@nthgergo/publishing-gh-pages-with-travis-ci-53a8270e87db
+set -o errexit
+
+rm -rf public
+mkdir public
+
+# config
+git config --global user.email "nobody@nobody.org"
+git config --global user.name "Travis CI"
+
+# build the documentation (CHANGE THIS)
+
+cd docs/
+make html
+cp -r build/html public/
+make clean
+
+# deploy
+cd public
+git init
+git add .
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
