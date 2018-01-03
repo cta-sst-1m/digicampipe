b1144c2c2f7f6b343facfbcbe518f9fb5d9c6e6a
Dominik Neise <neised@phys.ethz.ch>
2018-01-03 17:27:20 +0100

    move comment up

--- digicampipe/tests/test_lid_ccd.py
+++ digicampipe/tests/test_lid_ccd.py
@@ -38,12 +38,13 @@ def test_find_stars():
 
 
 def test_cone_simu():
+    # create an image  with a geometry compatible to the camera with
+    # known angle, spacing between pixels etc...
     test_image, true_positions = cones_simu(
         offset=(-4.3, -2.1),
         angle_deg=10,
         pixel_radius=35,
     )
-    # create an image  with a geometry compatible to the camera with known angle, spacing between pixels etc...
     cones_img = ConesImage(test_image)
     cones_img.get_cone(radius_mask=2.1, save_to_file=False)
     cones_img.fit_camera_geometry()
