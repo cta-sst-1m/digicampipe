cef41edf4bc6caa177477152b76e101ded4ac22d
Dominik Neise <neised@phys.ethz.ch>
2018-01-17 12:28:48 +0100

    set paths, so it works on my machine, and ...
     - add "camera=digicam" to event_stream
     - remove "camera=digicam" from dave_bias_curve

--- dark_evaluation.py
+++ dark_evaluation.py
@@ -11,10 +11,10 @@ import numpy as np
 if __name__ == '__main__':
     # Data configuration
 
-    directory = '/home/alispach/data/CRAB_01/'
-    filename = directory + 'CRAB_01_0_000.%03d.fits.fz'
+    directory = '/home/dneise/ctasoft/sst1m_crab/'
+    filename = directory + 'SST1M01_20171030.%03d.fits.fz'
     file_list = [filename % number for number in [2]]
-    camera_config_file = '/home/alispach/ctasoft/CTS/config/camera_config.cfg'
+    camera_config_file = '/home/dneise/ctasoft/digicampipe/digicampipe/tests/resources/camera_config.cfg'
     dark_filename = 'dark.npz'
 
     trigger_filename = 'bias_curve_dark.npz'
@@ -27,14 +27,13 @@ if __name__ == '__main__':
     digicam_geometry = geometry.generate_geometry_from_camera(camera=digicam)
 
     # Define the event stream
-    data_stream = event_stream(file_list=file_list, expert_mode=True, camera_geometry=digicam_geometry)
+    data_stream = event_stream(file_list=file_list, expert_mode=True, camera_geometry=digicam_geometry, camera=digicam)
     data_stream = filter.set_patches_to_zero(data_stream, unwanted_patch=unwanted_patch)
     # Fill the flags (to be replaced by Digicam)
     data_stream = filter.filter_event_types(data_stream, flags=[8])
 
     data_stream = save_bias_curve(data_stream,
                                   thresholds=thresholds,
-                                  camera=digicam,
                                   blinding=blinding,
                                   output_filename=directory + trigger_filename,
                                   unwanted_cluster=unwanted_cluster)
