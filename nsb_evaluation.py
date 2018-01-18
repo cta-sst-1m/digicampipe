025cafcccbbed06ad5f175bf41e23c138394c709
Dominik Neise <neised@phys.ethz.ch>
2018-01-18 18:43:12 +0100

    pep8

--- nsb_evaluation.py
+++ nsb_evaluation.py
@@ -94,10 +94,13 @@ if __name__ == '__main__':
         np.diff(data['time_stamp'] * 1E-9)[:, np.newaxis]
     )
 
-    mask = (baseline_max_min < 25) * (baseline_max_min > 0)
-    mask = mask * (baseline_max_dark > 55) * (baseline_max_dark < 60)
+    mask = (baseline_max_min < 25)
+    mask *= (baseline_max_min > 0)
+    mask *= (baseline_max_dark > 55)
+    mask *= (baseline_max_dark < 60)
     mask = mask[:, np.newaxis].T * np.abs(baseline_change) < 0.4
-    mask = mask * (data['baseline_shift'][:-1] > 30) * (data['baseline_shift'][:-1] < 80)
+    mask *= (data['baseline_shift'][:-1] > 30)
+    mask *= (data['baseline_shift'][:-1] < 80)
     x = data['nsb_rate'][:-1]
 
     x = np.ma.array(x, mask=~mask)
