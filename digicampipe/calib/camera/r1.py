5afc0b9749b10a21570490072f2e0cf85fb2e913
Dominik Neise <neised@phys.ethz.ch>
2018-01-19 11:11:18 +0100

    pep8

--- digicampipe/calib/camera/r1.py
+++ digicampipe/calib/camera/r1.py
@@ -15,21 +15,26 @@ def calibrate_to_r1(event_stream, dark_baseline):
             # Get the ADCs
             adc_samples = r0_camera.adc_samples
             baseline = r0_camera.baseline
-            adc_samples = adc_samples - baseline[:, np.newaxis].astype(np.int16)
+            adc_samples = (
+                adc_samples -
+                baseline[:, np.newaxis].astype(np.int16)
+            )
             r1_camera.adc_samples = adc_samples
             # Compute the gain drop and NSB
 
             if dark_baseline is None:
                 # compute NSB and Gain drop from STD
                 standard_deviation = r0_camera.standard_deviation
-                r1_camera.gain_drop = calib.compute_gain_drop(standard_deviation, 'std')
-                r1_camera.nsb = calib.compute_nsb_rate(standard_deviation, 'std')
+                r1_camera.gain_drop = calib.compute_gain_drop(
+                    standard_deviation, 'std')
+                r1_camera.nsb = calib.compute_nsb_rate(
+                    standard_deviation, 'std')
             else:
                 # compute NSB and Gain drop from baseline shift
                 r0_camera.dark_baseline = dark_baseline['baseline']
                 baseline_shift = baseline - r0_camera.dark_baseline
-                r1_camera.gain_drop = calib.compute_gain_drop(baseline_shift, 'mean')
+                r1_camera.gain_drop = calib.compute_gain_drop(
+                    baseline_shift, 'mean')
                 r1_camera.nsb = calib.compute_nsb_rate(baseline_shift, 'mean')
 
         yield event
-
