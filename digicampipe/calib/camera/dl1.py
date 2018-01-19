03fa50553d9ce75a9d80e392a23a84660c0a4ebf
Dominik Neise <neised@phys.ethz.ch>
2018-01-19 08:58:34 +0100

    pep8

--- digicampipe/calib/camera/dl1.py
+++ digicampipe/calib/camera/dl1.py
@@ -3,9 +3,13 @@ import numpy as np
 from ctapipe.image import cleaning
 
 
-def calibrate_to_dl1(event_stream, time_integration_options, picture_threshold=7,
-                                     boundary_threshold=4, additional_mask=None):
-
+def calibrate_to_dl1(
+    event_stream,
+    time_integration_options,
+    picture_threshold=7,
+    boundary_threshold=4,
+    additional_mask=None
+):
     for i, event in enumerate(event_stream):
 
         for telescope_id in event.r0.tels_with_data:
@@ -20,49 +24,71 @@ def calibrate_to_dl1(event_stream, time_integration_options, picture_threshold=7
 
             adc_samples = r1_camera.adc_samples
 
-            mask_for_cleaning = adc_samples > 3 * r0_camera.standard_deviation[:, np.newaxis]
+            mask_for_cleaning = (
+                adc_samples >
+                3 * r0_camera.standard_deviation[:, np.newaxis]
+            )
             dl1_camera.cleaning_mask = np.any(mask_for_cleaning, axis=-1)
             # dl1_camera.cleaning_mask *= (r1_camera.nsb < 5.) * (r1_camera.nsb > 0)
 
             adc_samples[~dl1_camera.cleaning_mask] = 0.
 
             gain_init = calib.get_gains()
-            gain = gain_init # * r1_camera.gain_drop
+            gain = gain_init  # * r1_camera.gain_drop
 
             # Integrate the data
-            adc_integrated = utils.integrate(adc_samples, time_integration_options['window_width'])
+            adc_integrated = utils.integrate(
+                adc_samples,
+                time_integration_options['window_width']
+            )
 
             pe_samples_trace = adc_integrated / gain[:, np.newaxis]
             n_samples = adc_samples.shape[-1]
-            dl1_camera.pe_samples_trace = np.pad(pe_samples_trace, ((0, 0), (0, n_samples - pe_samples_trace.shape[-1] % n_samples)), 'constant')
+            dl1_camera.pe_samples_trace = np.pad(
+                array=pe_samples_trace,
+                pad_width=(
+                    (0, 0),
+                    (0, n_samples - pe_samples_trace.shape[-1] % n_samples)
+                ),
+                mode='constant'
+            )
 
             # Compute the charge
-            dl1_camera.pe_samples, dl1_camera.time_bin = utils.extract_charge(adc_integrated,
-                                                                              time_integration_options['mask'],
-                                                                              time_integration_options['mask_edges'],
-                                                                              time_integration_options['peak'],
-                                                                              time_integration_options['window_start'],
-                                                                              time_integration_options['threshold_saturation'])
+            dl1_camera.pe_samples, dl1_camera.time_bin = utils.extract_charge(
+                adc_integrated,
+                time_integration_options['mask'],
+                time_integration_options['mask_edges'],
+                time_integration_options['peak'],
+                time_integration_options['window_start'],
+                time_integration_options['threshold_saturation']
+            )
             dl1_camera.pe_samples = dl1_camera.pe_samples / gain
 
             # mask pixels which goes above N sigma
 
-            dl1_camera.cleaning_mask *= cleaning.tailcuts_clean(geom=geom,
-                                                                image=dl1_camera.pe_samples,
-                                                                picture_thresh=picture_threshold,
-                                                                boundary_thresh=boundary_threshold,
-                                                                keep_isolated_pixels=False)
-                        
+            dl1_camera.cleaning_mask *= cleaning.tailcuts_clean(
+                geom=geom,
+                image=dl1_camera.pe_samples,
+                picture_thresh=picture_threshold,
+                boundary_thresh=boundary_threshold,
+                keep_isolated_pixels=False
+            )
+
             # recursive selection of neighboring pixels
             # threshold is 2*boundary_threshold, maybe we should introduce yet a 3rd threshold in the args of the function
-            image = dl1_camera.pe_samples 
+            image = dl1_camera.pe_samples
             recursion = True
             border = False
             while recursion:
                 recursion = False
                 for i in pixel_id[dl1_camera.cleaning_mask]:
                     num_neighbors = 0
-                    for j in pixel_id[geom.neighbor_matrix[i] & ~dl1_camera.cleaning_mask]:
+                    for j in (
+                        pixel_id[
+                            geom.neighbor_matrix[i] &
+                            (~dl1_camera.cleaning_mask)
+                        ]
+                    ):
                         num_neighbors = num_neighbors + 1
                         if image[j] > boundary_threshold:
                             dl1_camera.cleaning_mask[j] = True
@@ -72,14 +98,21 @@ def calibrate_to_dl1(event_stream, time_integration_options, picture_threshold=7
 
             dl1_camera.on_border = border
 
-            dl1_camera.cleaning_mask = cleaning.dilate(geom=geom, mask=dl1_camera.cleaning_mask)
+            dl1_camera.cleaning_mask = cleaning.dilate(
+                geom=geom,
+                mask=dl1_camera.cleaning_mask
+            )
 
             if additional_mask is not None:
-                dl1_camera.cleaning_mask = dl1_camera.cleaning_mask * additional_mask
+                dl1_camera.cleaning_mask *= additional_mask
 
             weight = dl1_camera.pe_samples
-            dl1_camera.time_spread = np.average(dl1_camera.time_bin[1] * 4, weights=weight)
-            dl1_camera.time_spread = np.average((dl1_camera.time_bin[1] * 4 - dl1_camera.time_spread)**2, weights=weight)
+            dl1_camera.time_spread = np.average(
+                dl1_camera.time_bin[1] * 4,
+                weights=weight)
+            dl1_camera.time_spread = np.average(
+                (dl1_camera.time_bin[1] * 4 - dl1_camera.time_spread)**2,
+                weights=weight)
             dl1_camera.time_spread = np.sqrt(dl1_camera.time_spread)
 
         yield event
