368d14ea4a727285ae92b25be885f4c0b8f43086
Dominik Neise <neised@phys.ethz.ch>
2018-01-19 17:23:28 +0100

    use ctapipe.io.zfits.ZFitsFileReader instead of our own version.

--- digicampipe/io/zfits.py
+++ digicampipe/io/zfits.py
@@ -3,20 +3,17 @@
 Components to read ZFITS data.
 This requires the protozfitsreader python library to be installed
 """
-import logging
-from digicampipe.io.containers import DataContainer
-import digicampipe.utils as utils
-from . import protozfitsreader
-logger = logging.getLogger(__name__)
-
+from ctapipe.io.zfits import ZFitsFileReader
+from digicampipe.utils import Camera as DigiCam
+import warning
 
 __all__ = ['zfits_event_source']
 
 
 def zfits_event_source(
     url,
-    camera_geometry,
-    camera,
+    camera=None,
+    camera_geometry=None,
     max_events=None,
     allowed_tels=None,
     expert_mode=False
@@ -38,57 +35,36 @@ def zfits_event_source(
     camera_geometry: CameraGeometry()
         camera containing info on pixels modules etc.
     """
+    if camera is None:
+        camera = DigiCam()
 
-    geometry = camera_geometry
-    patch_matrix = utils.geometry.compute_patch_matrix(camera=camera)
-    cluster_7_matrix = utils.geometry.compute_cluster_matrix_7(camera=camera)
-    cluster_19_matrix = utils.geometry.compute_cluster_matrix_19(camera=camera)
-    data = DataContainer()
+    if camera_geometry is not None:
+        warning.warn(
+            "camera_geometry is deprecated, "
+            "use digicampipe.utils.Camera",
+            DeprecationWarning
+        )
+    else:
+        camera_geometry = camera.geometry
 
-    loaded_telescopes = []
+    if allowed_tels is not None:
+        warning.warn(
+            "allowed_tels is deprecated, "
+            "I have no idea what you should do :-|",
+            DeprecationWarning
+        )
 
-    for event in protozfitsreader.ZFile(url):
-        if max_events is not None and event.event_id > max_events:
-            break
+    for event in ZFitsFileReader(input_url=url, max_events=max_events):
+        for tel_id in event.r0.tels_with_data:
+            event.inst.num_channels[tel_id] = event.num_channels
+            event.inst.num_pixels[tel_id] = event.n_pixels
+            event.inst.geom[tel_id] = camera_geometry
+            event.inst.cluster_matrix_7[tel_id] = camera.cluster_7_matrix
+            event.inst.cluster_matrix_19[tel_id] = camera.cluster_19_matrix
+            event.inst.patch_matrix[tel_id] = camera.patch_matrix
+            event.inst.num_samples[tel_id] = event.num_samples
 
-        data.r0.event_id = event.event_id
-        data.r0.tels_with_data = [event.telescope_id, ]
-
-        # remove forbidden telescopes
-        if allowed_tels:
-            data.r0.tels_with_data = [
-                list(filter(lambda x: x in data.r0.tels_with_data, sublist))
-                for sublist in allowed_tels
-            ]
-
-        for tel_id in data.r0.tels_with_data:
-
-            if tel_id not in loaded_telescopes:
-                data.inst.num_channels[tel_id] = event.num_channels
-                data.inst.num_pixels[tel_id] = event.n_pixels
-                data.inst.geom[tel_id] = geometry
-                data.inst.cluster_matrix_7[tel_id] = cluster_7_matrix
-                data.inst.cluster_matrix_19[tel_id] = cluster_19_matrix
-                data.inst.patch_matrix[tel_id] = patch_matrix
-                data.inst.num_samples[tel_id] = event.num_samples
-                loaded_telescopes.append(tel_id)
-
-            r0 = data.r0.tel[tel_id]
-            r0.camera_event_number = event.event_number
-            r0.pixel_flags = event.pixel_flags
-            r0.local_camera_clock = event.local_time
-            r0.gps_time = event.central_event_gps_time
-            r0.camera_event_type = event.camera_event_type
-            r0.array_event_type = event.array_event_type
-            r0.adc_samples = event.adc_samples
-
-            if expert_mode:
-                r0.trigger_input_traces = event.trigger_input_traces
-                r0.trigger_output_patch7 = event.trigger_output_patch7
-                r0.trigger_output_patch19 = event.trigger_output_patch19
-                r0.digicam_baseline = event.baseline
-
-        yield data
+        yield event
 
 
 def count_number_events(file_list):
