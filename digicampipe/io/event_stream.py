e43e0e8edb09cee37c0bc2c8997bdd6689a52f64
Dominik Neise <neised@phys.ethz.ch>
2018-02-13 09:48:04 +0100

    current state

--- digicampipe/io/event_stream.py
+++ digicampipe/io/event_stream.py
@@ -1,8 +1,7 @@
 from astropy.io import fits
 import numpy as np
-import pandas as pd
 from digicampipe.io import zfits, hdf5
-from digicampipe.io.slow_container import fill_slow
+from .auxservice import AuxService
 
 
 def event_stream(file_list, camera_geometry, camera,
@@ -27,81 +26,29 @@ def event_stream(file_list, camera_geometry, camera,
             yield event
 
 
-def get_foo(path):
-    data_struct = {}
-    hdulist = fits.open(path)
-    data_struct['hdu'] = hdulist[1]
-    ts = data_struct['hdu'].data['timestamp']
-    good = ts != 0
-    events = np.arange(len(ts))
-    data_struct['timestamps'] = ts[good]
-    data_struct['events'] = events[good]
-    data_struct['ts_min'] = min(data_struct['timestamps'])
-    data_struct['ts_max'] = max(data_struct['timestamps'])
-    data_struct['class_name'] = hdulist[1].header['SUBSYS']
-    return data_struct
+def add_slow_data(
+    data_stream,
+    aux_services=[
+        'DigicamSlowControl',
+        'MasterSST1M',
+        'PDPSlowControl',
+        'SafetyPLC',
+        'DriveSystem',
+    ],
+    basepath=None
+):
+    services = {
+        name: AuxService(name, basepath)
+        for name in aux_services
+    }
 
+    class SlowDataContainer:
+        pass
 
-def get_slow_data_info(file_list):
-    data_structs = {}
-    for file in file_list:
-        data_struct = get_foo(file)
-        class_name = data_struct['class_name']
-        if class_name not in data_structs:
-            data_structs[class_name] = [data_struct]
-        else:
-            data_structs[class_name].append(data_struct)
-    return data_structs
-
-
-def get_slow_event(info_struct_list, data_ts):
-    ts_min_all = [info_struct['ts_min'] for info_struct in info_struct_list]
-    ts_max_all = [info_struct['ts_max'] for info_struct in info_struct_list]
-    files = [info_struct['hdu']._file.name for info_struct in info_struct_list]
-    ts_min_all = np.array(ts_min_all)
-    ts_max_all = np.array(ts_max_all)
-    files = np.array(files)
-    indexes_slow_file = np.logical_and(
-        ts_min_all < data_ts,
-        ts_max_all > data_ts
-    )
-    if np.sum(indexes_slow_file) == 0:
-        # print('ERROR: no slow file found !')
-        return None, None
-    if np.sum(indexes_slow_file) > 1:
-        print('ERROR: several slow files found !',
-              files[indexes_slow_file])
-        return None, None
-    index_slow_file = np.argwhere(indexes_slow_file.flatten())[0, 0]
-    info_struct = info_struct_list[index_slow_file]
-    n_slow_event = len(info_struct['timestamps'])
-    # argmax stops at first True
-    index_next_slow_event = np.argmax(
-        info_struct['timestamps'] > data_ts
-    )
-    if index_next_slow_event == 0:
-        print('ERROR: wrong ts_min in get_slow_event()')
-        return None, None
-    index_slow_event = index_next_slow_event - 1
-    slow_event = info_struct['events'][index_slow_event]
-    hdu = info_struct['hdu']
-    return slow_event, hdu
-
-
-def add_slow_data(data_stream, slow_file_list):
-    slow_info_structs = get_slow_data_info(slow_file_list)
-    # now for each events look for the latest slow data
-    # with ts_slow<=ts_event
-    for event in data_stream:
-        if len(event.r0.tels_with_data) == 0:
-            print("WARNING: no R0 data in event")
-        telescope_id = event.r0.tels_with_data[0]
-        data_ts = event.r0.tel[telescope_id].local_camera_clock * 1e-6
-        for class_name in slow_info_structs.keys():
-            info_struct = slow_info_structs[class_name]
-            slow_event, hdu = get_slow_event(info_struct, data_ts)
-            if slow_event is None:
-                print("no %s data found" % class_name)
-                continue
-            event = fill_slow(class_name, event, hdu, slow_event)
+    for event_id, event in enumerate(data_stream):
+        slow_data_container = SlowDataContainer()
+        for aux_name in aux_services:
+            aux_row = services[aux_name].at(event.r0.tel[1].local_camera_clock)
+            setattr(slow_data_container, aux_name, aux_row)
+        event.slow_data = slow_data_container
         yield event
