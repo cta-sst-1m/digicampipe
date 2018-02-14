4e00cb019474e588bcd459b1ece64aef8814ff55
Dominik Neise <neised@phys.ethz.ch>
2018-02-14 12:34:41 +0100

    support also single files

--- digicampipe/io/event_stream.py
+++ digicampipe/io/event_stream.py
@@ -2,19 +2,24 @@ from digicampipe.io import zfits, hdf5
 
 
 def event_stream(
-    file_list,
+    filelist_or_url,
     camera=None,
     camera_geometry=None,
     expert_mode=None,
     max_events=None,
     mc=False
 ):
+    if isinstance(filelist_or_url, (str, bytes)):
+        filelist = [filelist_or_url]
+    else:
+        filelist = filelist_or_url
+
     if mc:
         source = hdf5.digicamtoy_event_source
     else:
         source = zfits.zfits_event_source
 
-    for file in file_list:
+    for file in filelist:
         data_stream = source(
             url=file,
             camera=camera,
