4b6bd663cfc52c95c47e70667743aff049de97e3
Dominik Neise <neised@phys.ethz.ch>
2018-02-14 11:37:49 +0100

    pep8

--- digicampipe/io/event_stream.py
+++ digicampipe/io/event_stream.py
@@ -1,25 +1,35 @@
 from digicampipe.io import zfits, hdf5
 
 
-def event_stream(file_list, camera_geometry, camera, expert_mode=False, max_events=None, mc=False):
+def event_stream(
+    file_list,
+    camera_geometry,
+    camera,
+    expert_mode=False,
+    max_events=None,
+    mc=False
+):
 
     for file in file_list:
 
         if not mc:
 
-            data_stream = zfits.zfits_event_source(url=file,
-                                                   expert_mode=expert_mode,
-                                                   camera_geometry=camera_geometry,
-                                                   max_events=max_events,
-                                                   camera=camera)
+            data_stream = zfits.zfits_event_source(
+                url=file,
+                expert_mode=expert_mode,
+                camera_geometry=camera_geometry,
+                max_events=max_events,
+                camera=camera
+            )
         else:
 
-            data_stream = hdf5.digicamtoy_event_source(url=file,
-                                                       camera_geometry=camera_geometry,
-                                                       camera=camera,
-                                                       max_events=max_events)
+            data_stream = hdf5.digicamtoy_event_source(
+                url=file,
+                camera_geometry=camera_geometry,
+                camera=camera,
+                max_events=max_events
+            )
 
         for event in data_stream:
 
             yield event
-
