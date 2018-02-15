8ba8ea9e886910ef1ac2bf70c6993cb7d95e113e
Dominik Neise <neised@phys.ethz.ch>
2018-02-15 22:36:18 +0100

    tidy this up

--- baseline_stability.py
+++ baseline_stability.py
@@ -1,73 +1,31 @@
-import numpy as np
+'''
+Something with baseline stability
+
+Usage:
+  baseline_stability <fitsfile>
+
+Options:
+  -h --help     Show this screen.
+'''
 from digicampipe.io.event_stream import event_stream
-import digicampipe.io.zfits as zfits
-from digicampipe.utils import DigiCam
-from digicamviewer.viewer import EventViewer
-from os.path import expanduser
-from optparse import OptionParser
 import matplotlib.pyplot as plt
-import os.path
+from docopt import docopt
 
 
-if __name__ == '__main__':
+def main(infile):
 
-    home_absolute_path = expanduser('~')
-    parser = OptionParser()
-    parser.add_option(
-        "-p", "--path", dest="directory", help="directory to data files",
-        default=os.path.realpath('digicampipe/tests/resources'))
-    parser.add_option(
-        '-f', '--file', dest='filename',
-        help='file basename e.g. CRAB_01_0_000.%03d.fits.fz',
-        default='/example_100_evts.%03d.fits.fz', type=str)
-    parser.add_option(
-        '-s', "--file_start", dest='file_start',
-        help='file number start', default=0, type=int)
-    parser.add_option(
-        '-e', "--file_end", dest='file_end',
-        help='file number end', default=0, type=int)
-
-    (options, args) = parser.parse_args()
-
-    # Input/Output files
-    directory = options.directory
-    filename = directory + options.filename
-    file_list = [
-        filename % number
-        for number in range(options.file_start, options.file_end + 1)
-    ]
-
-    # Noisy pixels not taken into account
-    # pixel_not_wanted = [
-    #    1038, 1039, 1002, 1003, 1004, 966, 967, 968, 930, 931, 932, 896]
-    n_events = zfits.count_number_events(file_list=file_list)
-    ####################
-    ##### ANALYSIS #####
-    ####################
-    baseline = np.zeros((len(DigiCam.geometry.pix_id), n_events))
-
-    # Define the event stream
-    data_stream = event_stream(
-        file_list=file_list,
-        camera=DigiCam
-    )
-
-    for i, data in zip(range(n_events), data_stream):
+    baselines = []
+    for data in event_stream(infile):
         for tel_id in data.r0.tels_with_data:
-            baseline[..., i] = data.r0.tel[tel_id].baseline
+            baselines.append(data.r0.tel[tel_id].baseline)
 
     plt.figure()
-    plt.plot(baseline[0])
-    plt.plot(baseline[1])
-    plt.plot(baseline[2])
-    plt.plot(baseline[1200])
+    plt.plot(baselines[0])
+    plt.plot(baselines[1])
+    plt.plot(baselines[2])
+    plt.plot(baselines[-1])
     plt.show()
 
-    display = EventViewer(
-        data_stream,
-        n_samples=50,
-        camera_config_file=DigiCam.config_file,
-        scale='lin',
-        # limits_colormap=[10, 500])
-    )
-    display.draw()
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    main(args)
