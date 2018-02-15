ecf13f45df393ef38a9ae02c5b0afc934c7d0cd1
Dominik Neise <neised@phys.ethz.ch>
2018-02-15 18:58:47 +0100

    remove stuff, which is not needed since new event_stream interface

--- baseline.py
+++ baseline.py
@@ -19,18 +19,16 @@ Options:
 from digicampipe.calib.camera import filter
 from digicampipe.io.event_stream import event_stream
 from digicampipe.io.save_adc import save_dark
-from digicampipe.utils import Camera
+from digicampipe.utils import DigiCam
 from tqdm import tqdm
 from docopt import docopt
 
 
 def main(baseline_file_path, files, unwanted_pixels=[]):
-    digicam = Camera()
+
     data_stream = event_stream(
         file_list=files,
-        expert_mode=True,
-        camera=digicam,
-        camera_geometry=digicam.geometry
+        camera=DigiCam,
     )
     data_stream = filter.set_pixels_to_zero(
         data_stream,
