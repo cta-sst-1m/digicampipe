99b16d8920cc9603fc77d137b06a45c7fffa88d0
Dominik Neise <neised@phys.ethz.ch>
2018-02-26 13:10:29 +0100

    pep8

--- digicampipe/visualization/__init__.py
+++ digicampipe/visualization/__init__.py
@@ -10,14 +10,25 @@ from ..utils import geometry, Camera
 
 
 class EventViewer():
-
-    def __init__(self, event_stream, camera_config_file, n_samples, scale='lin', limits_colormap=None, limits_readout=None, time_bin_start=0, pixel_id_start=0):
+    def __init__(
+        self,
+        event_stream,
+        camera_config_file,
+        n_samples,
+        scale='lin',
+        limits_colormap=None,
+        limits_readout=None,
+        time_bin_start=0,
+        pixel_id_start=0
+    ):
 
         mpl.figure.autolayout = False
         self.first_call = True
         self.event_stream = event_stream
         self.scale = scale
-        self.limits_colormap = limits_colormap if limits_colormap is not None else [-np.inf, np.inf]
+        self.limits_colormap = limits_colormap if limits_colormap is not None else [
+            -np.inf, np.inf
+        ]
         self.limits_readout = limits_readout
         self.time_bin = time_bin_start
         self.pixel_id = pixel_id_start
@@ -29,7 +40,9 @@ class EventViewer():
         self.geometry = geometry.generate_geometry(camera=self.camera)[0]
         self.n_pixels = len(self.camera.Pixels)
         self.n_samples = n_samples
-        self.cluster_matrix = np.zeros((len(self.camera.Clusters_7), len(self.camera.Clusters_7)))
+        self.cluster_matrix = np.zeros(
+            (len(self.camera.Clusters_7), len(self.camera.Clusters_7))
+        )
 
         for cluster in self.camera.Clusters_7:
 
@@ -45,13 +58,16 @@ class EventViewer():
         self.trigger_output = None
         self.trigger_input = None
         self.trigger_patch = None
-        self.nsb = [np.nan]*self.n_pixels
-        self.gain_drop = [np.nan]*self.n_pixels
-        self.baseline = [np.nan]*self.n_pixels
-        self.std = [np.nan]*self.n_pixels
+        self.nsb = [np.nan] * self.n_pixels
+        self.gain_drop = [np.nan] * self.n_pixels
+        self.baseline = [np.nan] * self.n_pixels
+        self.std = [np.nan] * self.n_pixels
         self.flag = None
 
-        self.readout_view_types = ['raw', 'baseline substracted', 'photon', 'trigger input', 'trigger output', 'cluster 7', 'reconstructed charge']
+        self.readout_view_types = [
+            'raw', 'baseline substracted', 'photon', 'trigger input',
+            'trigger output', 'cluster 7', 'reconstructed charge'
+        ]
         self.readout_view_type = 'raw'
 
         self.camera_view_types = ['sum', 'std', 'mean', 'max', 'time']
@@ -66,22 +82,39 @@ class EventViewer():
         self.axis_readout.set_ylabel('[ADC]')
         self.axis_readout.legend(loc='upper right')
         self.axis_readout.yaxis.set_major_formatter(FormatStrFormatter('%d'))
-        self.axis_readout.yaxis.set_major_locator(MaxNLocator(integer=True, nbins=10))
+        self.axis_readout.yaxis.set_major_locator(
+            MaxNLocator(integer=True, nbins=10)
+        )
 
-        self.trace_readout, = self.axis_readout.step(np.arange(self.n_samples) * 4, np.ones(self.n_samples), where='mid')
-        self.trace_time_plot, = self.axis_readout.plot(np.array([self.time_bin, self.time_bin]) * 4, np.ones(2), color='k',
-                                                       linestyle='--')
+        self.trace_readout, = self.axis_readout.step(
+            np.arange(self.n_samples) * 4,
+            np.ones(self.n_samples),
+            where='mid'
+        )
+        self.trace_time_plot, = self.axis_readout.plot(
+            np.array([self.time_bin, self.time_bin]) * 4,
+            np.ones(2),
+            color='k',
+            linestyle='--'
+        )
 
-        self.camera_visu = visualization.CameraDisplay(self.geometry, ax=self.axis_camera, title='', norm=self.scale,
-                                                       cmap='viridis',
-                                                       allow_pick=True)
+        self.camera_visu = visualization.CameraDisplay(
+            self.geometry,
+            ax=self.axis_camera,
+            title='',
+            norm=self.scale,
+            cmap='viridis',
+            allow_pick=True
+        )
 
         #if limits_colormap is not None:
         #    self.camera_visu.set_limits_minmax(limits_colormap[0], limits_colormap[1])
 
         self.camera_visu.image = np.zeros(self.n_pixels)
         self.camera_visu.cmap.set_bad(color='k')
-        self.camera_visu.add_colorbar(orientation='horizontal', pad=0.03, fraction=0.05, shrink=.85)
+        self.camera_visu.add_colorbar(
+            orientation='horizontal', pad=0.03, fraction=0.05, shrink=.85
+        )
 
         #if self.scale == 'log':
         #    self.camera_visu.colorbar.set_norm(LogNorm(vmin=1, vmax=None, clip=False))
@@ -91,25 +124,46 @@ class EventViewer():
         self.camera_visu.on_pixel_clicked = self.draw_readout
         self.camera_visu.pixels.set_snap(False)  # snap cursor to pixel center
 
-#        self.camera_visu.pixels.set_picker(False)
+        #        self.camera_visu.pixels.set_picker(False)
 
         # Buttons
 
-        self.axis_next_event_button = self.figure.add_axes([0.35, 0.9, 0.15, 0.07], zorder=np.inf)
-        self.axis_next_camera_view_button = self.figure.add_axes([0., 0.85, 0.1, 0.15], zorder=np.inf)
-        self.axis_next_view_type_button = self.figure.add_axes([0., 0.18, 0.1, 0.15], zorder=np.inf)
-        self.axis_check_button = self.figure.add_axes([0.35, 0.18, 0.1, 0.1], zorder=np.inf)
+        self.axis_next_event_button = self.figure.add_axes(
+            [0.35, 0.9, 0.15, 0.07], zorder=np.inf
+        )
+        self.axis_next_camera_view_button = self.figure.add_axes(
+            [0., 0.85, 0.1, 0.15], zorder=np.inf
+        )
+        self.axis_next_view_type_button = self.figure.add_axes(
+            [0., 0.18, 0.1, 0.15], zorder=np.inf
+        )
+        self.axis_check_button = self.figure.add_axes(
+            [0.35, 0.18, 0.1, 0.1], zorder=np.inf
+        )
         self.axis_next_camera_view_button.axis('off')
         self.axis_next_view_type_button.axis('off')
         self.button_next_event = Button(self.axis_next_event_button, 'Next')
-        self.radio_button_next_camera_view = RadioButtons(self.axis_next_camera_view_button, self.camera_view_types,
-                                                          active=self.camera_view_types.index(self.camera_view_type))
-        self.radio_button_next_view_type = RadioButtons(self.axis_next_view_type_button, self.readout_view_types,
-                                                        active=self.readout_view_types.index(self.readout_view_type))
+        self.radio_button_next_camera_view = RadioButtons(
+            self.axis_next_camera_view_button,
+            self.camera_view_types,
+            active=self.camera_view_types.index(self.camera_view_type)
+        )
+        self.radio_button_next_view_type = RadioButtons(
+            self.axis_next_view_type_button,
+            self.readout_view_types,
+            active=self.readout_view_types.index(self.readout_view_type)
+        )
 
-        self.check_button = CheckButtons(self.axis_check_button, ('mask', 'hillas'), (self.mask_pixels, self.hillas))
-        self.radio_button_next_view_type.set_active(self.readout_view_types.index(self.readout_view_type))
-        self.radio_button_next_camera_view.set_active(self.camera_view_types.index(self.camera_view_type))
+        self.check_button = CheckButtons(
+            self.axis_check_button, ('mask', 'hillas'),
+            (self.mask_pixels, self.hillas)
+        )
+        self.radio_button_next_view_type.set_active(
+            self.readout_view_types.index(self.readout_view_type)
+        )
+        self.radio_button_next_camera_view.set_active(
+            self.camera_view_types.index(self.camera_view_type)
+        )
 
     def next(self, event=None, step=1):
 
@@ -129,7 +183,9 @@ class EventViewer():
 
         try:
 
-            self.baseline = self.r0_container.baseline if np.isnan(self.r0_container.digicam_baseline).all() else self.r0_container.digicam_baseline
+            self.baseline = self.r0_container.baseline if np.isnan(
+                self.r0_container.digicam_baseline
+            ).all() else self.r0_container.digicam_baseline
             zero_image = np.zeros((self.n_pixels, self.n_samples))
 
             self.std = self.r0_container.standard_deviation if self.r0_container.standard_deviation is not None else np.nan * zero_image
@@ -151,7 +207,9 @@ class EventViewer():
 
         self.draw_readout(self.pixel_id)
         self.draw_camera()
-        self.button_next_event.label.set_text('Next : current event #%d' % (self.event_id))
+        self.button_next_event.label.set_text(
+            'Next : current event #%d' % (self.event_id)
+        )
 
     def draw(self):
 
@@ -174,7 +232,9 @@ class EventViewer():
     def draw_readout(self, pixel):
 
         y = self.compute_trace()[pixel]
-        limits_y = self.limits_readout if self.limits_readout is not None else [np.min(y), np.max(y) + 10]
+        limits_y = self.limits_readout if self.limits_readout is not None else [
+            np.min(y), np.max(y) + 10
+        ]
         self.pixel_id = pixel
         self.event_clicked_on.ind[-1] = self.pixel_id
         self.trace_readout.set_ydata(y)
@@ -194,7 +254,9 @@ class EventViewer():
 
         try:
 
-            legend += ' B = {:0.2f} [LSB],'.format(self.baseline[self.pixel_id])
+            legend += ' B = {:0.2f} [LSB],'.format(
+                self.baseline[self.pixel_id]
+            )
 
         except:
 
@@ -202,7 +264,9 @@ class EventViewer():
 
         try:
 
-            legend += ' $\sigma = $ {:0.2f} [LSB] \n'.format(self.std[self.pixel_id])
+            legend += ' $\sigma = $ {:0.2f} [LSB] \n'.format(
+                self.std[self.pixel_id]
+            )
 
         except:
 
@@ -210,8 +274,12 @@ class EventViewer():
 
         try:
 
-            legend += ' $G_{{drop}} = $ {:0.2f},'.format(self.gain_drop[self.pixel_id])
-            legend += ' $f_{{nsb}} = $ {:0.2f} [GHz]'.format(self.nsb[self.pixel_id])
+            legend += ' $G_{{drop}} = $ {:0.2f},'.format(
+                self.gain_drop[self.pixel_id]
+            )
+            legend += ' $f_{{nsb}} = $ {:0.2f} [GHz]'.format(
+                self.nsb[self.pixel_id]
+            )
 
         except:
 
@@ -241,16 +309,33 @@ class EventViewer():
 
             elif self.readout_view_type == 'trigger output' and self.trigger_output is not None:
 
-                image = np.array([self.trigger_output[pixel.patch] for pixel in self.camera.Pixels])
+                image = np.array(
+                    [
+                        self.trigger_output[pixel.patch]
+                        for pixel in self.camera.Pixels
+                    ]
+                )
 
             elif self.readout_view_type == 'trigger input' and self.trigger_input is not None:
 
-                image = np.array([self.trigger_input[pixel.patch] for pixel in self.camera.Pixels]) #np.zeros((self.n_pixels, self.n_samples))
+                image = np.array(
+                    [
+                        self.trigger_input[pixel.patch]
+                        for pixel in self.camera.Pixels
+                    ]
+                )  #np.zeros((self.n_pixels, self.n_samples))
 
             elif self.readout_view_type == 'cluster 7' and self.trigger_input is not None:
 
-                trigger_input_patch = np.dot(self.cluster_matrix, self.trigger_input)
-                image = np.array([trigger_input_patch[pixel.patch] for pixel in self.camera.Pixels])
+                trigger_input_patch = np.dot(
+                    self.cluster_matrix, self.trigger_input
+                )
+                image = np.array(
+                    [
+                        trigger_input_patch[pixel.patch]
+                        for pixel in self.camera.Pixels
+                    ]
+                )
 
             elif self.readout_view_type == 'photon' and self.dl1_container.pe_samples_trace is not None:
 
@@ -260,7 +345,10 @@ class EventViewer():
 
                 image = self.adc_samples - self.baseline[:, np.newaxis]
 
-            elif self.readout_view_type == 'reconstructed charge' and (self.dl1_container.time_bin is not None or self.dl1_container.pe_samples is not None):
+            elif self.readout_view_type == 'reconstructed charge' and (
+                self.dl1_container.time_bin is not None
+                or self.dl1_container.pe_samples is not None
+            ):
 
                 image = np.zeros((self.n_pixels, self.n_samples))
                 time_bins = self.dl1_container.time_bin
@@ -275,7 +363,7 @@ class EventViewer():
     def next_camera_view(self, camera_view, event=None):
 
         self.camera_view_type = camera_view
-        if self.readout_view_type in ['photon','reconstructed charge']:
+        if self.readout_view_type in ['photon', 'reconstructed charge']:
             self.camera_visu.colorbar.set_label('[p.e.]')
 
         else:
@@ -366,7 +454,8 @@ class EventViewer():
 
             mask = (self.image >= self.limits_colormap[0])
             if not self.limits_colormap[1] == np.inf:
-                image[(self.image > self.limits_colormap[1])] = self.limits_colormap[1]
+                image[(self.image > self.limits_colormap[1])
+                      ] = self.limits_colormap[1]
 
         if self.mask_pixels:
 
@@ -376,7 +465,9 @@ class EventViewer():
 
         if self.hillas:
 
-            self.camera_visu.overlay_moments(self.dl2_container.shower, color='r', linewidth=4)
+            self.camera_visu.overlay_moments(
+                self.dl2_container.shower, color='r', linewidth=4
+            )
 
         else:
 
@@ -413,6 +504,5 @@ class EventViewer():
 
 
 class EventClicked:
-
     def __init__(self, pixel_start):
         self.ind = [0, pixel_start]
