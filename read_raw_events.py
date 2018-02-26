c0438495d88f9827363f7c5849fb6474912d8678
Dominik Neise <neised@phys.ethz.ch>
2018-02-26 14:17:13 +0100

    remove a lot of noise from this example

--- read_raw_events.py
+++ read_raw_events.py
@@ -1,46 +1,15 @@
+'''
+Look at events in the EventViewer
+Usage:
+  digicamview <file>...
+'''
+from docopt import docopt
 from digicampipe.io import event_stream
-from cts_core.camera import Camera
-from digicampipe.utils import geometry
 from digicampipe.visualization import EventViewer
-import matplotlib.pyplot as plt
-from optparse import OptionParser
-from pkg_resources import resource_filename
-from os import path
-
-digicam_config_file = resource_filename(
-    'digicampipe',
-    path.join(
-        'tests',
-        'resources',
-        'camera_config.cfg'
-    )
-)
 
 
-if __name__ == '__main__':
-
-    parser = OptionParser()
-
-    parser.add_option(
-        "-d", "--directory", dest="directory", help="directory to data files",
-        default='/home/alispach/data/BASELINE_TEST/')
-    parser.add_option(
-        "-f", "--file",  dest="filename", help="filebasename e.g. CRAB_%03d.fits.fz",
-        default='BASELINE_TEST_0_000.%03d.fits.fz')
-    parser.add_option(
-        '-s', "--file_start", dest='file_start',
-        help='file number start', default=0)
-    parser.add_option(
-        '-e', "--file_end", dest='file_end',
-        help='file number end', default=23)
-
-    (options, args) = parser.parse_args()
-
-    directory = options.directory
-    filename = directory + options.filename
-    file_list = [filename % number for number in range(options.file_start, options.file_end + 1)]
-
-    data_stream = event_stream.event_stream(file_list=file_list)
-    with plt.style.context('ggplot'):
-        display = EventViewer(data_stream)
-        display.draw()
+def entry():
+    args = docopt(__doc__)
+    data_stream = event_stream.event_stream(args['<file>'])
+    display = EventViewer(data_stream)
+    display.draw()
