aa1162f6727cb75043f83f588b2aa2c3e44ced3d
Cyril Alispach <cyril.alispach@unige.ch>
2018-03-06 10:05:52 +0100

    removed CameraCalibration.extract_charge

--- digicampipe/instrument/camera.py
+++ digicampipe/instrument/camera.py
@@ -97,48 +97,6 @@ class CameraCalibration:
             self.lut_nsb_rate
         )
 
-    def extract_charge(
-        self,
-        data,
-        timing_mask,
-        timing_mask_edge,
-        peak,
-        window_start,
-        threshold_saturation
-    ):
-        """
-        Extract the charge.
-           - check which pixels are saturated
-           - get the local maximum within the timing mask and check
-             if it is not at the edge of the mask
-           - move window_start from the maximum
-        :param data:
-        :param timing_mask:
-        :param timing_mask_edge:
-        :param peak_position:
-        :param options:
-        :param integration_type:
-        :return:
-        """
-
-        is_saturated = np.max(data, axis=-1) > threshold_saturation
-        local_max = np.argmax(np.multiply(data, timing_mask), axis=1)
-        local_max_edge = np.argmax(np.multiply(data, timing_mask_edge), axis=1)
-        ind_max_at_edge = (local_max == local_max_edge)
-        local_max[ind_max_at_edge] = peak[ind_max_at_edge] - window_start
-        index_max = (np.arange(0, data.shape[0]), local_max,)
-        ind_with_lt_th = data[index_max] < 10.
-        local_max[ind_with_lt_th] = peak[ind_with_lt_th] - window_start
-        local_max[local_max < 0] = 0
-        index_max = (np.arange(0, data.shape[0]), local_max,)
-        charge = data[index_max]
-        if np.any(is_saturated):  ## TODO, find a better evaluation that it is saturated
-            sat_indices = tuple(np.where(is_saturated)[0])
-            _data = data[sat_indices, ...]
-            charge[sat_indices, ...] = np.apply_along_axis(contiguous_regions, 1, _data)
-
-        return charge, index_max
-
     def contiguous_regions(data):
         """Finds contiguous True regions of the boolean array "condition". Returns
         a 2D array where the first column is the start index of the region and the
