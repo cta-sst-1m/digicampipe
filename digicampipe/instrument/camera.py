31fdae1a14ef21c389ed6a74178ff0cf4180dfba
Cyril Alispach <cyril.alispach@unige.ch>
2018-03-06 10:06:38 +0100

    removed CameraCalibration.contigous_regions

--- digicampipe/instrument/camera.py
+++ digicampipe/instrument/camera.py
@@ -97,35 +97,6 @@ class CameraCalibration:
             self.lut_nsb_rate
         )
 
-    def contiguous_regions(data):
-        """Finds contiguous True regions of the boolean array "condition". Returns
-        a 2D array where the first column is the start index of the region and the
-        second column is the end index."""
-        condition = data > 0
-        # Find the indicies of changes in "condition"
-        d = np.diff(condition)
-        idx, = d.nonzero()
-
-        # We need to start things after the change in "condition". Therefore,
-        # we'll shift the index by 1 to the right.
-        idx += 1
-
-        if condition[0]:
-            # If the start of condition is True prepend a 0
-            idx = np.r_[0, idx]
-
-        if condition[-1]:
-            # If the end of condition is True, append the length of the array
-            idx = np.r_[idx, condition.size]  # Edit
-
-        # Reshape the result into two columns
-        idx.shape = (-1, 2)
-        val = 0.
-        for start, stop in idx:
-            sum_tmp = np.sum(data[start:stop])
-            if val < sum_tmp: val = sum_tmp
-        return val
-
     def fake_timing_hist(n_samples, timing_width, central_sample):
         """
         Create a timing array based on options.central_sample and options.timing_width
