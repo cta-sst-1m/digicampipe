e70d048422d3e61a313141664110e4a1fe32d5dc
Cyril Alispach <cyril.alispach@unige.ch>
2018-03-12 11:27:24 +0100

    spe_fit added, cleanup to come

--- spe.py
+++ spe.py
@@ -18,6 +18,7 @@ from tqdm import tqdm
 import peakutils
 from scipy.signal import find_peaks_cwt
 from scipy import ndimage
+from scipy.interpolate import splrep, sproot
 import scipy
 from iminuit import Minuit
 from digicampipe.io.containers import CalibrationContainer
@@ -160,25 +161,81 @@ def compute_fit_init_param(x, y, snr=8, sigma_e=None):
         raise FirstPhotoElectronPeakNotFound('Could not detect any peak in the'
                                              'histogram')
 
-    amplitudes = np.zeros(4)
-    for i, peak_index in enumerate(peak_indices[1:]):
+    peaks_y = np.array(y[peak_indices])
+    peaks_x = np.array(x[peak_indices])
 
-        if i < amplitudes.shape[0]:
-            amplitudes[i] = y[..., peak_index]
+    peaks_y = np.insert(peaks_y, 0, 0)
+    peaks_x = np.insert(peaks_x, 0, 0)
 
-    gain = np.diff(x[peak_indices])
+    print(peaks_y, peaks_x)
+
+    gain = np.diff(peaks_x)
     gain = np.mean(gain)
 
-    temp = np.argmax(y[peak_indices])
-    one_pe_peak_index = peak_indices[temp]
-    baseline = x[peak_indices[temp - 1]]
+    print(gain)
+
+    temp = np.argmax(peaks_y)
+    one_pe_peak_x = peaks_x[temp]
+    zero_pe_peak_x = peaks_x[temp - 1]
+
+    baseline = zero_pe_peak_x
+    print(x, zero_pe_peak_x)
+    one_pe_peak_index = np.where(x == one_pe_peak_x)[0][0]
+    print(one_pe_peak_index)
 
     x_right = (gain / 2).astype(np.int)
-    one_pe_peak_region = [one_pe_peak_index + i for i in range(0, x_right)]
-    interp = scipy.interpolate.interp1d(y[..., one_pe_peak_region], x[..., one_pe_peak_region], kind='cubic')
+    print(x_right)
+    one_pe_peak_region = [one_pe_peak_index + i for i in range(-x_right, x_right)]
+    print(one_pe_peak_region)
+    # y_max = np.max(y[one_pe_peak_region])
+    # s = splrep(x[one_pe_peak_region], y[one_pe_peak_region] - y_max / 2, k=3)
+    # roots = sproot(s)
 
-    fwhm = interp(y[..., one_pe_peak_index]/2) - x[..., one_pe_peak_index]
-    sigma = fwhm / 2.355
+    # print(roots)
+
+
+    # interp = scipy.interpolate.interp1d(y[..., one_pe_peak_region], x[..., one_pe_peak_region], kind='cubic')
+
+    # fwhm = interp(y[..., one_pe_peak_index]/2) - x[..., one_pe_peak_index]
+    # fwhm = np.abs(roots[1] - roots[0])
+    # sigma = fwhm / 2.355
+
+    x_1 = x[one_pe_peak_region]
+    y_1 = y[one_pe_peak_region]
+    mean_x_1 = np.average(x_1, weights=y_1)
+    std_x_1 = np.average((x_1 - mean_x_1)**2, weights=y_1)
+    std_x_1 = np.sqrt(std_x_1)
+    n_entries = np.sum(y_1)
+
+
+    keys = [
+        'limit_mean', 'limit_sigma', 'limit_amplitude'
+    ]
+
+    values = [
+        (0, x_1.max()),
+        (0, x_1.max()),
+        (n_entries * 0.5, n_entries * 1.5)
+    ]
+
+    param_bounds = dict(zip(keys, values))
+
+    params_init = {'mean': mean_x_1, 'sigma': std_x_1, 'amplitude': n_entries}
+
+    # plt.figure()
+    # plt.plot(x, y)
+    # plt.plot(x[peak_indices], y[peak_indices], linestyle='None', marker='o')
+    # plt.plot(x_1, y_1, linestyle='None', marker='o')
+    # plt.plot(x_1, gaussian(x_1, **params_init))
+    # plt.show()
+
+    f = lambda mean, sigma, amplitude: minimiser(x_1, y_1, np.sqrt(y_1), gaussian, mean, sigma, amplitude)
+    m = Minuit(f, **params_init, **param_bounds, print_level=0, pedantic=False)
+    m.migrad()
+
+    # plt.plot(x, gaussian(x, **m.values))
+
+    sigma = m.values['sigma']
 
     if sigma_e is None:
 
@@ -189,19 +246,13 @@ def compute_fit_init_param(x, y, snr=8, sigma_e=None):
 
         sigma_s = np.sqrt(sigma**2 - sigma_e**2)
 
-    plt.figure()
-    plt.plot(x, y)
-    plt.plot(x[peak_indices], y[peak_indices], linestyle='None', marker='o')
-    plt.plot(x[..., one_pe_peak_region], y[one_pe_peak_region], linestyle='None', marker='o')
-    plt.show()
-
     keys = ['baseline', 'gain', 'sigma_e', 'sigma_s']
-    for i in range(1, len(amplitudes)):
+    for i in range(1, peaks_y.shape[0]):
 
         keys.append('a_{}'.format(i))
 
     values = [baseline, gain, sigma_e, sigma_s]
-    values = values + amplitudes.tolist()
+    values = values + peaks_y[1:].tolist() # append to list
 
     return dict(zip(keys, values))
 
@@ -238,14 +289,29 @@ def fit_spe(x, y, y_err):
     param_bounds = dict(zip(keys, values))
 
     f = lambda baseline, gain, sigma_e, sigma_s, a_1, a_2, a_3, a_4: minimiser(x, y, y_err, spe_fit_function, baseline, gain, sigma_e, sigma_s, a_1, a_2, a_3, a_4)
-    m = Minuit(f, **params_init, **param_bounds)
+    m = Minuit(f, **params_init, **param_bounds, print_level=0, pedantic=False)
     m.migrad()
-    m.minos()
 
-    plt.plot(x, y)
-    plt.plot(x, spe_fit_function(x, **m.values))
+    try:
+        m.minos()
+    except RuntimeError:
+        pass
 
-    return
+    # plt.plot(x, y)
+    # plt.plot(x, spe_fit_function(x, **m.values))
+    # plt.show()
+
+    return m.values, m.errors
+
+
+def gaussian(x, mean, sigma, amplitude):
+
+    pdf = (x - mean)**2 / (2 * sigma**2)
+    pdf = np.exp(-pdf)
+    pdf /= np.sqrt(2 * np.pi) * sigma
+    pdf *= amplitude
+
+    return pdf
 
 
 def minimiser(x, y, y_err, f, *args):
@@ -253,24 +319,23 @@ def minimiser(x, y, y_err, f, *args):
     return np.sum(((y - f(x, *args)) / y_err)**2)
 
 
-def main(args):
+def compute_histo(filename):
 
-    files = args['<files>']
-
-    events = calibration_event_stream(files, telescope_id=1)
+    events = calibration_event_stream(filename, telescope_id=1)
     mean, std, mode, max = compute_event_stats(events)
 
-    events = calibration_event_stream(files, telescope_id=1)
+    events = calibration_event_stream(filename, telescope_id=1)
     events = subtract_baseline(events, mode)
     # events = normalize_adc_samples(events, std)
     # events = find_pulse_1(events, 0.5, 20)
-    events = find_pulse_2(events, widths=[5, 6], threshold=2*std)
+    events = find_pulse_2(events, widths=[5, 6], threshold=2 * std)
     # events = normalize_adc_samples(events, 1./std)
     events = compute_charge(events, width=7)
     events = compute_amplitude(events)
 
-    spe = Histogram1D(data_shape=(1296, ), bin_edges=np.arange(-20, 200, 1))
-    spe_amplitude = Histogram1D(data_shape=(1296, ), bin_edges=np.arange(-20, 200, 1))
+    spe = Histogram1D(data_shape=(1296,), bin_edges=np.arange(-20, 200, 1))
+    spe_amplitude = Histogram1D(data_shape=(1296,),
+                                bin_edges=np.arange(-20, 200, 1))
 
     plt.figure()
 
@@ -282,7 +347,6 @@ def main(args):
     # window_template = window_template[window_template > 0]
 
     for event, i in tqdm(zip(events, range(10000)), total=10000):
-
         spe.fill(event.reconstructed_charge)
         spe_amplitude.fill(event.reconstructed_amplitude)
         # print(event.reconstructed_charge)
@@ -295,16 +359,53 @@ def main(args):
     spe.save('temp.pk')
     spe.save('temp_amplitute.pk')
 
+
+def main(args):
+
+    files = args['<files>']
+
+
+    # compute_histo(files)
+
     spe = Histogram1D.load('temp.pk')
     print(spe.data)
 
-    spe.draw(index=(10, ))
+    spe.draw(index=(10, ), log=True)
     plt.show()
 
-    snr = 8
-    temp = spe.data.sum(axis=0)
+    # snr = 8
+    # temp = spe.data.sum(axis=0)
 
-    fit_spe(spe._bin_centers(),temp, np.sqrt(temp))
+    parameters = {'a_1': [], 'a_2':[], 'a_3':[], 'a_4':[], 'sigma_s': [], 'sigma_e':[], 'gain':[], 'baseline':[], 'pixel_id':[]}
+    parameters_error = []
+
+    for pixel_id in range(spe.data.shape[0]):
+
+        try:
+            params, params_err = fit_spe(
+                spe._bin_centers(),
+                spe.data[pixel_id],
+                spe.errors(index=pixel_id))
+
+            for key, val in params.items():
+                parameters[key].append(val)
+
+            parameters['pixel_id'].append(pixel_id)
+
+        except FirstPhotoElectronPeakNotFound:
+
+            print('Could not fit for pixel_id : {}'.format(pixel_id))
+
+    plt.figure()
+    plt.hist(parameters['sigma_e'], bins='auto')
+
+    plt.figure()
+    plt.hist(parameters['gain'], bins='auto')
+
+    plt.figure()
+    plt.hist(parameters['sigma_s'], bins='auto')
+
+    plt.show()
 
     return
 
