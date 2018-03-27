7837b9113d668b1d0e93f409452c30c6d423055b
Dominik Neise <neised@phys.ethz.ch>
2018-03-27 10:52:48 +0200

    add a unit test for calibration_event_stream

---
+++ digicampipe/tests/test_calibration_container.py
@@ -0,0 +1,32 @@
+from digicampipe.io.event_stream import calibration_event_stream, event_stream
+import pkg_resources
+import os
+
+example_file_path = pkg_resources.resource_filename(
+    'digicampipe',
+    os.path.join(
+        'tests',
+        'resources',
+        'example_100_evts.000.fits.fz'
+    )
+)
+
+
+def test_calibration_event_stream():
+
+    path = example_file_path
+    max_events = 10
+    telescope_id = 1
+
+    calib_stream = calibration_event_stream(path, telescope_id, max_events)
+    obs_stream = event_stream(path, max_events=max_events)
+
+    for event_calib, event_obs in zip(calib_stream, obs_stream):
+
+        assert (event_calib.data.adc_samples == \
+               event_obs.r0.tel[telescope_id].adc_samples).all()
+        assert (event_calib.data.digicam_baseline == \
+               event_obs.r0.tel[telescope_id].digicam_baseline).all()
+
+        assert event_calib.n_pixels == \
+               event_obs.r0.tel[telescope_id].adc_samples.shape[0]
