edbe89fffdfd6d26c84f63b9c942a94d5c94173b
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 16:36:27 +0200

    this is not needed anymore

--- digicampipe/tests/test_zfits_event_source.py
+++ digicampipe/tests/test_zfits_event_source.py
@@ -1,13 +1,6 @@
 from digicampipe.io.zfits import zfits_event_source
 import pkg_resources
 import os
-import warnings
-
-from cts_core.camera import Camera
-from digicampipe.utils import geometry
-
-
-warnings.simplefilter("ignore")
 
 example_file_path = pkg_resources.resource_filename(
     'digicampipe',
@@ -17,18 +10,6 @@ example_file_path = pkg_resources.resource_filename(
         'example_100_evts.000.fits.fz'
     )
 )
-
-digicam_config_file = pkg_resources.resource_filename(
-    'digicampipe',
-    os.path.join(
-        'tests',
-        'resources',
-        'camera_config.cfg'
-    )
-)
-
-digicam = Camera(_config_file=digicam_config_file)
-digicam_geometry = geometry.generate_geometry_from_camera(camera=digicam)
 
 
 def test_and_benchmark_event_source(benchmark):
