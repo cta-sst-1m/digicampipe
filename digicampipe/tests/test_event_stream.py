d17d46c0893dce3284f09aeff63f766edcd7b51c
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 16:11:56 +0200

    this cannot work anymore

--- digicampipe/tests/test_event_stream.py
+++ digicampipe/tests/test_event_stream.py
@@ -28,15 +28,6 @@ digicam = Camera(_config_file=digicam_config_file)
 digicam_geometry = geometry.generate_geometry_from_camera(camera=digicam)
 
 
-def test_event_source_old_style():
-
-    for _ in event_stream(
-            [example_file_path],
-            camera=digicam,
-            camera_geometry=digicam_geometry):
-        pass
-
-
 def test_event_source_new_style():
 
     for _ in event_stream(example_file_path):
