cbff05198b83c1b846ef924a886c2875922c7ce5
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 16:35:27 +0200

    there is no expert and basic anymore

--- digicampipe/tests/test_zfits_event_source.py
+++ digicampipe/tests/test_zfits_event_source.py
@@ -31,27 +31,11 @@ digicam = Camera(_config_file=digicam_config_file)
 digicam_geometry = geometry.generate_geometry_from_camera(camera=digicam)
 
 
-def test_and_benchmark_simple_event_source(benchmark):
+def test_and_benchmark_event_source(benchmark):
 
     @benchmark
-    def simple_loop():
-        for _ in zfits_event_source(
-                example_file_path,
-                camera=digicam,
-                camera_geometry=digicam_geometry,
-                expert_mode=False,):
-            pass
-
-
-def test_and_benchmark_expert_event_source(benchmark):
-
-    @benchmark
-    def expert_loop():
-        for _ in zfits_event_source(
-                example_file_path,
-                camera=digicam,
-                camera_geometry=digicam_geometry,
-                expert_mode=True,):
+    def loop():
+        for _ in zfits_event_source(example_file_path):
             pass
 
 
