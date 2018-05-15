27f6887f0594145381191dd333615d34872f01f4
Dominik Neise <neised@phys.ethz.ch>
2018-05-15 16:14:01 +0200

    this is all not needed anymore

--- digicampipe/tests/test_event_stream.py
+++ digicampipe/tests/test_event_stream.py
@@ -2,10 +2,6 @@ from digicampipe.io.event_stream import event_stream
 import pkg_resources
 import os
 
-from cts_core.camera import Camera
-from digicampipe.utils import geometry
-
-
 example_file_path = pkg_resources.resource_filename(
     'digicampipe',
     os.path.join(
@@ -14,18 +10,6 @@ example_file_path = pkg_resources.resource_filename(
         'example_100_evts.000.fits.fz'
     )
 )
-
-digicam_config_file = pkg_resources.resource_filename(
-    'digicampipe',
-    os.path.join(
-        'tests',
-        'resources',
-        'camera_config.cfg'
-    )
-)
-
-digicam = Camera(_config_file=digicam_config_file)
-digicam_geometry = geometry.generate_geometry_from_camera(camera=digicam)
 
 
 def test_event_source_new_style():
