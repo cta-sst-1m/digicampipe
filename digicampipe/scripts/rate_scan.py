043714b17c2a6694a08674d6accc48bf3efcdcc0
Cyril Alispach <cyril.alispach@unige.ch>
2018-05-23 20:18:55 +0200

    improved CLI for rate_scan

--- digicampipe/scripts/rate_scan.py
+++ digicampipe/scripts/rate_scan.py
@@ -7,6 +7,7 @@ Usage:
 
 Options:
   --display   Display the plots
+  --compute   Computes the trigger rate vs threshold
   -o OUTPUT --output=OUTPUT.  Folder where to store the results.
   -i INPUT --input=INPUT.     Input files.
 '''
@@ -20,18 +21,10 @@ from digicampipe.io.save_bias_curve import compute_bias_curve, \
 from digicampipe.io.event_stream import event_stream
 
 
-def entry():
-
-    args = docopt(__doc__)
-    print(args['INPUT'])
-    main(args['INPUT'], args['OUTPUT'], args['--display'])
-
-
-def main(files, output_filename, display=False):
+def compute(files, output_filename):
 
     n_bins = 1024
     thresholds = np.arange(0, 100, 2)
-    blinding = True
 
     data_stream = event_stream(files)
     data_stream = r0.fill_event_type(data_stream, flag=8)
@@ -40,7 +33,7 @@ def main(files, output_filename, display=False):
     data_stream = r0.fill_trigger_patch(data_stream)
     data_stream = r0.fill_trigger_input_7(data_stream)
     data_stream = r0.fill_trigger_input_19(data_stream)
-    output = compute_bias_curve_v2(
+    output = compute_bias_curve(
         data_stream,
         thresholds=thresholds,
     )
@@ -51,7 +44,28 @@ def main(files, output_filename, display=False):
              cluster_rate=cluster_rate, cluster_rate_error=cluster_rate_error,
              thresholds=thresholds)
 
-    if display:
+    return output
+
+
+def entry():
+
+    args = docopt(__doc__)
+    input_files = args['INPUT']
+    output_file = args['OUTPUT']
+
+    if args['--compute']:
+
+        compute(input_files, output_file)
+
+    if args['--display']:
+
+
+        output = np.load(output_file)
+
+        thresholds = output['thresholds']
+        rate = output['rate']
+        rate_error = output['rate_error']
+
 
         fig = plt.figure()
         axes = fig.add_subplot(111)
