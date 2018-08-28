3127a8f0e52b51a37466ed8448db7d4507216ab6
Yves Renier <yves.renier@unige.ch>
2018-08-28 17:36:55 +0200

    same fix for data quality

--- digicampipe/tests/test_pipeline.py
+++ digicampipe/tests/test_pipeline.py
@@ -36,36 +36,36 @@ expected_columns = ['phi', 'y', 'skewness', 'intensity', 'x', 'event_id',
 
 def test_pipeline():
     # checks that the pipeline produce a fits file with all columns
-    with tempfile.TemporaryFile() as dark_file:
-        dark_filename = str(dark_file.name)
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        dark_filename = os.path.join(tmpdirname, 'dark.pk')
         compute_raw(
             files=[example_file1_path], 
             max_events=None, 
             pixel_id=convert_pixel_args(None), 
             filename=dark_filename
         )
-        with tempfile.TemporaryDirectory() as tmpdirname:
-            main_pipeline(
-                files=[example_file2_path],
-                max_events=None, 
-                dark_filename=dark_filename, 
-                pixel_ids=convert_pixel_args(None), 
-                shift=0, 
-                integral_width=7,
-                debug=False,
-                output_path=tmpdirname, 
-                parameters_filename=calibration_filename, 
-                compute=True, 
-                display=False,
-                picture_threshold=1, # unusual value, so events pass cuts
-                boundary_threshold=1 # unusual value, so events pass cuts
-            )
-            hdul = fits.open(os.path.join(tmpdirname, 'hillas.fits'))
-            cols = [c.name for c in hdul[1].columns]
-            nevent = len(hdul[1].data['local_time'])
-            for col in expected_columns:
-                assert col in cols
-                assert len(hdul[1].data[col]) == nevent
+
+        main_pipeline(
+            files=[example_file2_path],
+            max_events=None,
+            dark_filename=dark_filename,
+            pixel_ids=convert_pixel_args(None),
+            shift=0,
+            integral_width=7,
+            debug=False,
+            output_path=tmpdirname,
+            parameters_filename=calibration_filename,
+            compute=True,
+            display=False,
+            picture_threshold=1, # unusual value, so events pass cuts
+            boundary_threshold=1 # unusual value, so events pass cuts
+        )
+        hdul = fits.open(os.path.join(tmpdirname, 'hillas.fits'))
+        cols = [c.name for c in hdul[1].columns]
+        nevent = len(hdul[1].data['local_time'])
+        for col in expected_columns:
+            assert col in cols
+            assert len(hdul[1].data[col]) == nevent
 
 
 if __name__ == '__main__':
