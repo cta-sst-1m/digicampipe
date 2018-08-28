da9ee709b0655f1fb9989165a506c0ae8e20bfde
Yves Renier <yves.renier@unige.ch>
2018-08-28 13:27:41 +0200

    start to fix catpipe>=6 compatibility

--- digicampipe/calib/camera/image.py
+++ digicampipe/calib/camera/image.py
@@ -1,5 +1,5 @@
 from ctapipe.image import hillas_parameters
-
+from ctapipe import __version__ as ctapipe_version
 
 def compute_hillas_parameters(events, geom):
 
@@ -8,8 +8,10 @@ def compute_hillas_parameters(events, geom):
         mask = event.data.cleaning_mask
         image = event.data.reconstructed_number_of_pe
         image[~mask] = 0
-
-        hillas = hillas_parameters(geom, image, container=True)
+        if ctapipe_version < '0.6':
+            hillas = hillas_parameters(geom, image, container=True)
+        else:
+            hillas = hillas_parameters(geom, image)
         event.hillas = hillas
 
         yield event
