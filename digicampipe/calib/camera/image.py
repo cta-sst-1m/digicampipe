7d7a37c5341329e51e04abf08360c588eccac59f
Yves Renier <yves.renier@unige.ch>
2018-08-28 14:40:06 +0200

    avoid compatibility patch and inforce ctapipe>=0.6

--- digicampipe/calib/camera/image.py
+++ digicampipe/calib/camera/image.py
@@ -1,5 +1,5 @@
 from ctapipe.image import hillas_parameters
-from ctapipe import __version__ as ctapipe_version
+
 
 def compute_hillas_parameters(events, geom):
 
@@ -8,10 +8,7 @@ def compute_hillas_parameters(events, geom):
         mask = event.data.cleaning_mask
         image = event.data.reconstructed_number_of_pe
         image[~mask] = 0
-        if ctapipe_version < '0.6':
-            hillas = hillas_parameters(geom, image, container=True)
-        else:
-            hillas = hillas_parameters(geom, image)
+        hillas = hillas_parameters(geom, image)
         event.hillas = hillas
 
         yield event
