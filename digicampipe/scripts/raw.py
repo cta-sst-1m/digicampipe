e0b352867236f9c2a91732479051f7701b89916c
Yves Renier <yves.renier@unige.ch>
2018-08-29 14:58:03 +0200

    allow to select filename in raw script. Also that script skip calculation only if no input files are given.

--- digicampipe/scripts/raw.py
+++ digicampipe/scripts/raw.py
@@ -8,13 +8,12 @@ Usage:
 Options:
   -h --help                   Show this screen.
   --max_events=N              Maximum number of events to analyse
-  -o OUTPUT --output=OUTPUT.  Folder where to store the results.
+  -o FILE --output=FILE.      File where to store the results.
                               [Default: .]
   -c --compute                Compute the data.
   -d --display                Display.
-  -v --debug                  Enter the debug mode.
   -p --pixel=<PIXEL>          Give a list of pixel IDs.
-  --save_figures              Save the plots to the OUTPUT folder
+  --save_figures              Save the plots to the same folder as output file.
 '''
 import os
 from docopt import docopt
@@ -30,18 +29,13 @@ from digicampipe.utils.docopt import convert_max_events_args,\
 
 def compute(files, max_events, pixel_id, filename):
 
-    if os.path.exists(filename):
-
+    if os.path.exists(filename) and len(files) == 0:
         raw_histo = Histogram1D.load(filename)
-
         return raw_histo
-
     else:
-
         n_pixels = len(pixel_id)
         events = calibration_event_stream(files, pixel_id=pixel_id,
                                           max_events=max_events)
-
         raw_histo = Histogram1D(
             data_shape=(n_pixels,),
             bin_edges=np.arange(0, 4095, 1),
@@ -49,9 +43,7 @@ def compute(files, max_events, pixel_id, filename):
 
         for event in events:
             raw_histo.fill(event.data.adc_samples)
-
         raw_histo.save(filename)
-
         return raw_histo
 
 
@@ -59,24 +51,20 @@ def entry():
 
     args = docopt(__doc__)
     files = args['<INPUT>']
-    debug = args['--debug']
 
     max_events = convert_max_events_args(args['--max_events'])
     pixel_id = convert_pixel_args(args['--pixel'])
-    output_path = args['--output']
-    raw_histo_filename = os.path.join(output_path, 'raw_histo.pk')
+    raw_histo_filename = args['--output']
 
+    output_path = os.path.basename(raw_histo_filename)
     if not os.path.exists(output_path):
-
         raise IOError('Path {} for output '
                       'does not exists \n'.format(output_path))
 
     if args['--compute']:
-
         compute(files, max_events, pixel_id, raw_histo_filename)
 
     if args['--save_figures']:
-
         raw_histo = Histogram1D.load(raw_histo_filename)
 
         path = os.path.join(output_path, 'figures/', 'raw_histo/')
