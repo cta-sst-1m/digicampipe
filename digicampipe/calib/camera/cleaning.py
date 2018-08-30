552bb4c06139f831317ee8494b2fed17e06742fd
Yves Renier <yves.renier@unige.ch>
2018-08-30 14:25:06 +0200

    add compute_3d_cleaning()

--- digicampipe/calib/camera/cleaning.py
+++ digicampipe/calib/camera/cleaning.py
@@ -82,3 +82,40 @@ def compute_dilate(events, geom):
         event.data.cleaning_mask = mask
 
         yield event
+
+
+def compute_3d_cleaning(events, geom, threshold_pe_frac=20, threshold_time=2.1,
+                        threshold_size=0.005, n_sample=50, sampling_time=4,
+                        n_pixel=1296):
+    samples = np.arange(0, n_sample * sampling_time, sampling_time)
+    pix_x = np.tile(geom.pix_x.value[:, None], [1, n_sample])
+    pix_y = np.tile(geom.pix_y.value[:, None], [1, n_sample])
+    pix_t = np.tile(samples[None, :], [n_pixel, 1])
+    for event in events:
+        pe_frac = event.data.reconstructed_fraction_of_pe
+        # ignore missing pixels
+        pe_frac[~np.isfinite(pe_frac)] = 0
+        # set to 0 the samples with fractional pe lower than threshold_pe_frac
+        pe_frac[pe_frac < threshold_pe_frac] = 0
+        sum_t = np.nansum(pe_frac, axis=1)
+        mean_t = np.nansum(pe_frac * pix_t, axis=1) / sum_t
+        mean_t_tiled = np.tile(mean_t[:, None], [1, n_sample])
+        var_t = np.nansum(pe_frac * (pix_t - mean_t_tiled) ** 2 , axis=1) / \
+                sum_t
+        # set fractional pe to 0 for pixels with too short pulses
+        pe_frac[np.sqrt(var_t) < threshold_time, :] = 0
+
+        sum_pixel_pe = np.nansum(pe_frac, axis=0)
+        mean_x = np.nansum(pe_frac * pix_x, axis=0) / sum_pixel_pe
+        mean_y = np.nansum(pe_frac * pix_y, axis=0) / sum_pixel_pe
+        mean_x_tiled = np.tile(mean_x[None, :], [n_pixel, 1])
+        mean_y_tiled = np.tile(mean_y[None, :], [n_pixel, 1])
+        var_x = np.nansum(pe_frac * (pix_x - mean_x_tiled) ** 2 , axis=0) / \
+                sum_pixel_pe
+        var_y = np.nansum(pe_frac * (pix_y - mean_y_tiled) ** 2 , axis=0) / \
+                sum_pixel_pe
+        shower = False
+        if np.logical_and(np.isfinite(var_x + var_y), var_x + var_y > 0):
+            std_xy = np.mean(np.sqrt(var_x + var_y))
+            shower = std_xy > threshold_size
+        event.data.shower = shower
