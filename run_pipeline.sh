de6a4878dc248fa2a194bb6f80ec49bc964ab89d
Cyril Alispach <cyril.alispach@unige.ch>
2018-09-07 16:29:31 +0200

    just change the pipeline script, will not commit anymore

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-# digicam-raw --compute --output=./ /sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_{001..003}.fits.fz
-digicam-pipeline --compute --display --output=. --dark=raw_histo.pk --parameters=digicampipe/tests/resources/calibration_20180814.yml /sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_{009..010}.fits.fz --template=digicampipe/tests/resources/pulse_SST-1M_pixel_0.dat --debug
+dark_files=(/sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_001.fits.fz /sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_002.fits.fz /sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_003.fits.fz
+)
+science_files=(/sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_009.fits.fz /sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_010.fits.fz
+)
+
+digicam-raw --compute --output=./ $dark_files
+digicam-pipeline --compute --display --output=. --dark=raw_histo.pk --parameters=digicampipe/tests/resources/calibration_20180814.yml --template=digicampipe/tests/resources/pulse_SST-1M_pixel_0.dat $science_files
