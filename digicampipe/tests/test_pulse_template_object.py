ba738ba4b1548e85c29583e3d1d618e23576aa5e
Cyril Alispach <cyril.alispach@unige.ch>
2018-09-07 16:45:16 +0200

    creating tests for the template

---
+++ digicampipe/tests/test_pulse_template_object.py
@@ -0,0 +1,11 @@
+from pkg_resource import resource_filename
+import os
+
+template_filename = resource_filename(
+    'digicampipe',
+    os.path.join(
+        'tests',
+        'resources',
+        'pulse_SST-1M_pixel_0.dat'
+    )
+
