de1611ae84aa181e2c9385af052d92bb08c09a2c
Cyril Alispach <cyril.alispach@unige.ch>
2018-09-10 10:23:09 +0200

    pipeline script update

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -6,5 +6,7 @@ dark_files=(/sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_001.fits.fz /sst1m/
 science_files=(/sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_009.fits.fz /sst1m/raw/2018/08/19/SST1M_01/SST1M_01_20180819_010.fits.fz
 )
 
+
+
 digicam-raw --compute --output=./ $dark_files
-digicam-pipeline --compute --display --output=. --dark=raw_histo.pk --parameters=digicampipe/tests/resources/calibration_20180814.yml --template=digicampipe/tests/resources/pulse_SST-1M_pixel_0.dat $science_files
+digicam-pipeline --compute --display --output=./hillas.fits --dark=raw_histo.pk --parameters=digicampipe/tests/resources/calibration_20180814.yml --template=digicampipe/tests/resources/pulse_SST-1M_pixel_0.dat $science_files
