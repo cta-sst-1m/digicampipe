e686dd5b59c46ae296fd621ceb2d369eefed4c24
Dominik Neise <neised@phys.ethz.ch>
2018-09-14 10:51:06 +0200

    and entry() to plt_rate_vs_threshold

--- digicampipe/scripts/plt_rate_vs_threshold.py
+++ digicampipe/scripts/plt_rate_vs_threshold.py
@@ -8,20 +8,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 from docopt import docopt
 
-args = docopt(__doc__)
-fig = plt.figure()
-for path in args['<trigger_npz_file>']:
-    file_name = split(path)[-1][:-4]
-    file = np.load(path)
-    plt.errorbar(
-        file['thresholds'],
-        file['rate'] * 1E9,
-        yerr=file['rate_error'] * 1E9,
-        label=file_name)
 
-plt.ylabel('rate [Hz]')
-plt.xlabel('threshold [LSB]')
-plt.yscale('log')
-plt.grid(True)
-plt.legend(loc='best')
-plt.show()
+def entry():
+    args = docopt(__doc__)
+    fig = plt.figure()
+    for path in args['<trigger_npz_file>']:
+        file_name = split(path)[-1][:-4]
+        file = np.load(path)
+        plt.errorbar(
+            file['thresholds'],
+            file['rate'] * 1E9,
+            yerr=file['rate_error'] * 1E9,
+            label=file_name)
+
+    plt.ylabel('rate [Hz]')
+    plt.xlabel('threshold [LSB]')
+    plt.yscale('log')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.show()
+
+
+if __name__ == '__main__':
+    entry()
