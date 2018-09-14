c16557a2b35b17590602a5cb70f7a12770474696
Yves Renier <yves.renier@unige.ch>
2018-09-14 21:48:51 +0200

    add entry() to get_burst script

--- digicampipe/scripts/get_burst.py
+++ digicampipe/scripts/get_burst.py
@@ -20,9 +20,13 @@ Options:
   --expand=INT                  Each burst get expended by the specified number
                                 of events
                                 [Default: 10]
+  --plot_baseline=FILE          Path to the outpout plot of the histroy of the 
+                                mean baseline. Set to "none" to not make that 
+                                plot or to "show" to display it.
+                                [Default: show]
   --merge_sec=FLOAT             Merge bursts if they are closer than the
                                 specified amount of seconds
-                                [Default: 5]
+                                [Default: 5.0]
 """
 import numpy as np
 import sys
@@ -30,6 +34,8 @@ import pandas as pd
 from docopt import docopt
 from digicampipe.io.event_stream import calibration_event_stream
 from digicampipe.calib.camera.baseline import fill_digicam_baseline, tag_burst
+from matplotlib import pyplot as plt
+from pandas import to_datetime
 
 def expand_mask(input, iters=1):
     """
@@ -46,85 +52,102 @@ def expand_mask(input, iters=1):
     return output
 
 
-args = docopt(__doc__)
-files = args['<INPUT>']
-event_average = int(args['--event_average'])
-threshold_lsb = float(args['--threshold_lsb'])
-output = args['--output']
-expand = int(args['--expand'])
-merge_sec = float(args['--expand'])
+def entry(files, plot_baseline="show", event_average=100, threshold_lsb=2., 
+          output="none", expand=10, merge_sec=5.):
+    # get events info
+    events = calibration_event_stream(files)
+    events = fill_digicam_baseline(events)
+    events = tag_burst(events, event_average=event_average, 
+                       threshold_lsb=threshold_lsb)
+    n_event = 0
+    timestamps = []
+    event_ids = []
+    are_burst = []
+    baselines = []
+    for event in events:
+        n_event += 1
+        timestamps.append(event.data.local_time)
+        event_ids.append(event.event_id)
+        are_burst.append(event.data.burst)
+        baselines.append(np.mean(event.data.digicam_baseline))
+    timestamps = np.array(timestamps)
+    event_ids = np.array(event_ids)
+    are_burst = np.array(are_burst)
+    baselines = np.array(baselines)
+    
+    # plot history of the baselines
+    if plot_baseline.lower() != "none":
+        fig1 = plt.figure(figsize=(8,6))
+        plt.plot_date(to_datetime(timestamps), baselines, '.')
+        plt.ylabel('mean baseline [LSB]')
+        if plot_baseline.lower() == "show":
+            plt.show()
+        else:
+            plt.savefig(plot_baseline)
+        plt.close(fig1)
 
-# get events info
-events = calibration_event_stream(files)
-events = fill_digicam_baseline(events)
-events = tag_burst(events, event_average=event_average, 
-                   threshold_lsb=threshold_lsb)
-n_event = 0
-timestamps = []
-event_ids = []
-are_burst = []
-for event in events:
-    n_event += 1
-    timestamps.append(event.data.local_time)
-    event_ids.append(event.event_id)
-    are_burst.append(event.data.burst)
-timestamps = np.array(timestamps)
-event_ids = np.array(event_ids)
-are_burst = np.array(are_burst)
-if np.all(~are_burst):
-    raise SystemExit('no burst detected')
+    # identify the bursts
+    if np.all(~are_burst):
+        raise SystemExit('no burst detected')
+    are_burst = expand_mask(are_burst, iters=expand)
+    previous_is_burst = False
+    bursts = []
+    for event in range(n_event):
+        if are_burst[event]:
+            if not previous_is_burst:
+                bursts.append([event])
+            if event == n_event - 1 or (not are_burst[event + 1]):
+                bursts[-1].append(event)
+            previous_is_burst = True
+        else:
+            previous_is_burst = False
+    if np.all(~are_burst):
+        raise SystemExit('no burst identified')
 
-# identify the bursts
-are_burst = expand_mask(are_burst, iters=expand)
-previous_is_burst = False
-bursts = []
-for event in range(n_event):
-    if are_burst[event]:
-        if not previous_is_burst:
-            bursts.append([event])
-        if event == n_event - 1 or (not are_burst[event + 1]):
-            bursts[-1].append(event)
-        previous_is_burst = True
-    else:
-        previous_is_burst = False
-if np.all(~are_burst):
-    raise SystemExit('no burst identified')
-
-# merge bursts which are closer than merge_sec seconds
-last_burst_begin = bursts[0][0]
-last_burst_end = bursts[0][1]
-merged_bursts = []
-n_burst = len(bursts)
-for burst_idxs in bursts[1:]:
-    begin_idx, end_idx = burst_idxs
-    if (timestamps[begin_idx] - timestamps[last_burst_end]) < merge_sec * 1e9:
-        last_burst_end = end_idx
-    else:
+    # merge bursts which are closer than merge_sec seconds
+    last_burst_begin = bursts[0][0]
+    last_burst_end = bursts[0][1]
+    merged_bursts = []
+    n_burst = len(bursts)
+    for burst_idxs in bursts[1:]:
+        begin_idx, end_idx = burst_idxs
+        if (timestamps[begin_idx] - timestamps[last_burst_end]) < merge_sec * 1e9:
+            last_burst_end = end_idx
+        else:
+            merged_bursts.append([last_burst_begin, last_burst_end])
+            last_burst_begin = begin_idx
+            last_burst_end = end_idx
+    if len(merged_bursts) == 0 or merged_bursts[-1][0] != last_burst_begin:
         merged_bursts.append([last_burst_begin, last_burst_end])
-        last_burst_begin = begin_idx
-        last_burst_end = end_idx
-if len(merged_bursts) == 0 or merged_bursts[-1][0] != last_burst_begin:
-    merged_bursts.append([last_burst_begin, last_burst_end])
-# print('merged_burst:')
-# for i, burst in enumerate(merged_bursts):
-#     beg, end = burst
-#     print(i, beg, end)
-bursts = merged_bursts
+    bursts = merged_bursts
 
-# output result
-if output == "none":
-    run_file = sys.stdout
-else:
-    run_file = open(output, 'w')
-run_file.write("#burst ts_start ts_end id_start id_end\n")  # write header
-date_format = '%Y-%m-%dT%H:%M:%S'
-for i, burst_idxs in enumerate(bursts):
+    # output result
+    if output == "none":
+        run_file = sys.stdout
+    else:
+        run_file = open(output, 'w')
+    run_file.write("#burst ts_start ts_end id_start id_end\n")  # write header
+    date_format = '%Y-%m-%dT%H:%M:%S'
+    for i, burst_idxs in enumerate(bursts):
+        begin_idx, end_idx = burst_idxs
+        ts_begin = pd.to_datetime(timestamps[begin_idx]).strftime(date_format)
+        ts_end = pd.to_datetime(timestamps[end_idx]).strftime(date_format)
+        run_file.write(str(i) + " " + ts_begin + " " + ts_end)
+        run_file.write(" " + str(event_ids[begin_idx]) + " ")
+        run_file.write(str(event_ids[end_idx]) + "\n")
 
-    ts_begin = pd.to_datetime(timestamps[begin_idx]).strftime(date_format)
-    ts_end = pd.to_datetime(timestamps[end_idx]).strftime(date_format)
-    run_file.write(str(i) + " " + ts_begin + " " + ts_end)
-    run_file.write(" " + str(event_ids[begin_idx]) + " ")
-    run_file.write(str(event_ids[end_idx]) + "\n")
+    if output != "none":
+        run_file.close()
 
-if output != "none":
-    run_file.close()
+if __name__ == '__main__':
+    args = docopt(__doc__)
+    files = args['<INPUT>']
+    event_average = int(args['--event_average'])
+    threshold_lsb = float(args['--threshold_lsb'])
+    output = args['--output']
+    expand = int(args['--expand'])
+    merge_sec = float(args['--merge_sec'])
+    plot_baseline = args['--plot_baseline']
+    entry(files, plot_baseline, event_average, threshold_lsb, output, expand, 
+          merge_sec)
+
