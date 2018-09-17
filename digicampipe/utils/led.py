28b2c6d31b1027f8da0bdf60a788ccf755e84566
Cyril Alispach <cyril.alispach@unige.ch>
2018-09-17 18:13:28 +0200

    include errors into fit if needed

--- digicampipe/utils/led.py
+++ digicampipe/utils/led.py
@@ -1,19 +1,20 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import warnings
 
 
 class ACLEDInterpolator:
 
-    def __init__(self, ac_level, photo_electrons):
+    def __init__(self, ac_level, photo_electrons, photo_electrons_err=None):
 
         self.ac_level = ac_level
         self.photo_electrons = photo_electrons
-        self._template = self._interpolate()
+        self.photo_electrons_err = photo_electrons_err
+        self._params = self._interpolate()
 
-    def __call__(self, ac_level, pixel=None):
+    def __call__(self, ac_level, pixel=0):
 
-        y = self._template(ac_level)[pixel]
-
+        y = np.polyval(self._params[pixel], ac_level)
         return y
 
     def __getitem__(self, item):
@@ -28,29 +29,83 @@ class ACLEDInterpolator:
 
         return cls(amplitude=x, time=t)
 
-    def _interpolate(self):
+    def _interpolate(self, deg=4):
 
-        ac_level = self.ac_level
-        pe = self.photo_electrons
+        pes = self.photo_electrons.copy()
+        params = []
 
-        mask = (pe > 1) * (pe < 200)
-        pe = np.ma.masked_array(pe, mask=mask)
-        param = np.polyfit(ac_level, pe, deg=4)
+        for i, pe in enumerate(pes.T):
 
-        return lambda x: np.polyval(param, x)
+            ac_level = self.ac_level.copy()
+            mask = (pe > 0) * (pe < 200) * np.isfinite(pe)
 
-    def plot(self, axes=None, pixel=None, **kwargs):
+            err = None
+            if self.photo_electrons_err is not None:
+                err = self.photo_electrons_err[:, i]
+                mask = mask * (np.isfinite(err))
+                err = err[mask]
+                err = 1 / err
+
+            pe = pe[mask]
+            ac_level = ac_level[mask]
+
+            if len(pe) <= deg+1:
+
+                param = [np.nan] * (deg + 1)
+
+                warnings.warn('Could not interpolate pixel {}'.format(i),
+                              UserWarning)
+
+            else:
+                param = np.polyfit(ac_level, pe, deg=deg, w=err)
+
+            params.append(param)
+
+        params = np.array(params)
+        return params
+
+    def plot(self, axes=None, pixel=0, **kwargs):
 
         if axes is None:
             fig = plt.figure()
             axes = fig.add_subplot(111)
 
         x_fit = np.arange(1000)
-        y_fit = self(x_fit)[pixel]
+        y_fit = self(x_fit, pixel=pixel)
 
-        axes.plot(self.ac_level, self.photo_electrons, label='Data points',
-                  linestyle='None', marker='o', color='k', **kwargs)
-        axes.plot(x_fit, y_fit, label='Interpolated template', color='r')
+        mask = (y_fit > 0) * (y_fit <= 2000)
+        x_fit = x_fit[mask]
+        y_fit = y_fit[mask]
+
+        if self.photo_electrons_err is not None:
+
+            y_err = self.photo_electrons_err[:, pixel]
+        else:
+
+            y_err = None
+
+        axes.errorbar(self.ac_level, self.photo_electrons[:, pixel],
+                      yerr=y_err, label='Data points, pixel : {}'.format(pixel),
+                      linestyle='None', marker='o', color='k', **kwargs)
+        axes.plot(x_fit, y_fit, label='Interpolated data', color='r')
+        axes.set_xlabel('AC DAC level')
+        axes.set_ylabel('Number of p.e.')
+        axes.set_yscale('log')
         axes.legend(loc='best')
 
         return axes
+
+
+if __name__ == '__main__':
+
+    data = np.load('/home/alispach/ctasoft/digicampipe/charge_linearity.npz')
+    ac_leds = np.load('/home/alispach/data/tests/mpe/mpe_fit_results.npz')
+
+    ac_levels = ac_leds['ac_levels']
+    pe = ac_leds['mu']
+    pe_err = ac_leds['mu_error']
+
+    test = ACLEDInterpolator(ac_levels, pe, pe_err)
+
+    test.plot(pixel=559)
+    plt.show()
