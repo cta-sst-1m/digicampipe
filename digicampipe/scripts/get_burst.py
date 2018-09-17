2de4e5caec3a6b9608513bfd8237a353c652e396
Yves Renier <yves.renier@unige.ch>
2018-09-17 10:57:30 +0200

    add possibility to save video of baselines during burst

--- digicampipe/scripts/get_burst.py
+++ digicampipe/scripts/get_burst.py
@@ -12,7 +12,7 @@ Options:
                                 average for the event to be taken as part of a 
                                 burst
                                 [Default: 2.0]
-  --output=FILE                Path to the output text file listing the bursts
+  --output=FILE                 Path to the output text file listing the bursts
                                 and giving the range of timetamps and event_ids
                                 for each. If "none", it goes to standard 
                                 outpout.
@@ -27,6 +27,13 @@ Options:
   --merge_sec=FLOAT             Merge bursts if they are closer than the
                                 specified amount of seconds
                                 [Default: 5.0]
+  --video_prefix=FILE           Prefix of the output video files. One video per
+                                burst (with path=prefix + "_" + str(burst_idx))
+                                The videos show the baseline evolution during
+                                a burst. Set to "none" to not make any video or
+                                set to "show" to display them.
+                                [Default: none]
+
 """
 import numpy as np
 import sys
@@ -36,6 +43,10 @@ from digicampipe.io.event_stream import calibration_event_stream
 from digicampipe.calib.camera.baseline import fill_digicam_baseline, tag_burst
 from matplotlib import pyplot as plt
 from pandas import to_datetime
+from ctapipe.visualization import CameraDisplay
+from digicampipe.utils import DigiCam
+import matplotlib.animation as animation
+
 
 def expand_mask(input, iters=1):
     """
@@ -52,8 +63,63 @@ def expand_mask(input, iters=1):
     return output
 
 
+def animate_baseline(events, video, event_id_min=None, event_id_max=None):
+    fig = plt.figure()
+    ax = plt.gca()
+    display = CameraDisplay(DigiCam.geometry, ax=ax)
+    display.add_colorbar()
+    title = plt.title("")
+    event_ids = []
+    ts = []
+    baselines = []
+    for event in events:
+        if event_id_min and event.event_id <= event_id_min:
+            continue
+        if event_id_max and event.event_id >= event_id_max:
+            break
+        event_ids.append(event.event_id)
+        ts.append(event.data.local_time)
+        baselines.append(event.data.digicam_baseline)
+    t0 = ts[0]
+    title_text = 'Baseline evolution\n' + 'event: %i, t=%.3f s' % \
+                 (event_ids[0], np.round((ts[0] - t0) * 1e-9, 3))
+    title.set_text(title_text)
+    display.image = baselines[0]
+
+    def update(i):
+        event_id = event_ids[i]
+        t = ts[i]
+        title_text = 'Baseline evolution\n' + 'event: %i, t=%.3f s' \
+                     %(event_id, np.round((t - t0) * 1e-9, 3))
+        title.set_text(title_text)
+        display.image = baselines[i]
+        plt.pause(.04)
+        return display, title
+
+    nframe = len(ts)
+    print('creating animation with',nframe , 'frames')
+    anim = animation.FuncAnimation(fig, update, save_count=nframe + 1,
+                                   frames=np.arange(1, nframe),
+                                   interval=100)
+    print('saving...')
+    plt.rcParams['animation.ffmpeg_path'] = \
+        u'/home/yves/anaconda3/envs/digicampipe/bin/ffmpeg'
+    if video != "show":
+        # import logging
+        # logger = logging.getLogger('matplotlib.animation')
+        # logger.setLevel(logging.DEBUG)
+        anim.save(video, writer='ffmpeg',
+                  metadata=dict(artist='yves.renier@unige.ch',
+                                comment='baseline during burst'),
+                  codec='mpeg4', bitrate=20000)
+        print('video saved as', video)
+    else:
+        plt.show()
+    plt.close(fig)
+
+
 def entry(files, plot_baseline="show", event_average=100, threshold_lsb=2., 
-          output="none", expand=10, merge_sec=5.):
+          output="none", expand=10, merge_sec=5., video_prefix="none"):
     # get events info
     events = calibration_event_stream(files)
     events = fill_digicam_baseline(events)
@@ -135,9 +201,19 @@ def entry(files, plot_baseline="show", event_average=100, threshold_lsb=2.,
         run_file.write(str(i) + " " + ts_begin + " " + ts_end)
         run_file.write(" " + str(event_ids[begin_idx]) + " ")
         run_file.write(str(event_ids[end_idx]) + "\n")
-
+        if video_prefix != "none":
+            first_event_id = event_ids[begin_idx]
+            events = calibration_event_stream(files, event_id=first_event_id)
+            events = fill_digicam_baseline(events)
+            if video_prefix != "show":
+                video = video_prefix + "_" + str(i) + ".mp4"
+            else:
+                video = "show"
+            animate_baseline(events, video, event_id_min=event_ids[begin_idx],
+                             event_id_max=event_ids[end_idx])
     if output != "none":
         run_file.close()
+
 
 if __name__ == '__main__':
     args = docopt(__doc__)
@@ -148,6 +224,7 @@ if __name__ == '__main__':
     expand = int(args['--expand'])
     merge_sec = float(args['--merge_sec'])
     plot_baseline = args['--plot_baseline']
+    video_prefix = args['--video_prefix']
     entry(files, plot_baseline, event_average, threshold_lsb, output, expand, 
-          merge_sec)
+          merge_sec, video_prefix)
 
