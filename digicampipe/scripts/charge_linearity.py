fixes in std computation

digicampipe/scripts/charge_linearity.py
@@ -9,11 +9,11 @@ from tqdm import tqdm
 
 files = ['/sst1m/raw/2018/06/28/SST1M_01/SST1M_01_20180628_{}.fits.fz' \
          ''.format(i) for i in range(1350, 1454 + 1, 1)]
-files = files
+files = files[50:52]
 ac_levels = np.hstack([np.arange(0, 20, 1), np.arange(20, 450, 5)])
 n_pixels = 1296
 n_files = len(files)
-filename = 'charge_linearity.npz'
+filename = 'charge_linearity_final.npz'
 
 debug = False
 
@@ -39,22 +39,27 @@ for i, file in tqdm(enumerate(files), total=n_files):
         charge_mean[i] += event.data.reconstructed_charge
         amplitude_mean[i] += event.data.reconstructed_amplitude
 
-        charge_std[i] += charge_mean[i]**2
-        amplitude_std[i] += amplitude_mean[i]**2
+        charge_std[i] += event.data.reconstructed_charge**2
+        amplitude_std[i] += event.data.reconstructed_amplitude**2
 
     charge_mean[i] = charge_mean[i] / (n + 1)
     charge_std[i] = charge_std[i] / (n + 1)
     charge_std[i] = np.sqrt(charge_std[i] - charge_mean[i]**2)
     amplitude_mean[i] = amplitude_mean[i] / (n + 1)
     amplitude_std[i] = amplitude_std[i] / (n + 1)
-    amplitude_std[i] = np.sqrt(amplitude_std[i] - amplitude_std[i]**2)
+    amplitude_std[i] = np.sqrt(amplitude_std[i] - amplitude_mean[i]**2)
+
+print(charge_std, charge_mean)
 
 np.savez(filename, charge_mean=charge_mean, charge_std=charge_std,
-         amplitude_mean=amplitude_mean,
-         amplitude_std=amplitude_std, ac_levels=ac_levels)
+         amplitude_mean=amplitude_mean, amplitude_std=amplitude_std,
+         ac_levels=ac_levels)
 
 
 plt.figure()
 plt.scatter(charge_mean.ravel(), amplitude_mean.ravel())
 
 plt.show()
+
+plt.figure()
+plt.plot(charge_mean.ravel(), charge_mean.ravel()/charge_std.ravel())

digicampipe/utils/led.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import warnings
+from scipy.interpolate import interp1d
 
 
 class ACLEDInterpolator:
@@ -10,17 +11,28 @@ class ACLEDInterpolator:
         self.ac_level = ac_level
         self.photo_electrons = photo_electrons
         self.photo_electrons_err = photo_electrons_err
-        self._params = self._interpolate()
+        self._params, self._spline = self._interpolate()
+        self.interpolation_end_x = np.max(self.ac_level, axis=-1)
+        self.interpolation_start_x = np.min(self.ac_level, axis=-1)
 
     def __call__(self, ac_level, pixel=None):
 
-        if pixel is None:
+        y = self._spline(ac_level)
 
-            y = np.polyval(self._params.T, ac_level[:, np.newaxis]).T
+        y_extrapolated = np.polyval(self._params.T, ac_level[:, np.newaxis]).T
 
-        else:
+        if pixel is not None:
 
-            y = np.polyval(self._params[pixel], ac_level)
+            y = y[pixel]
+            y_extrapolated = y_extrapolated[pixel]
+
+        y_shape = y.shape
+        y = y.ravel()
+
+        extrapolated_values = np.isnan(y)
+        y_extrapolated = y_extrapolated.ravel()
+        y[extrapolated_values] = y_extrapolated[extrapolated_values]
+        y = y.reshape(y_shape)
 
         return y
 
@@ -38,6 +50,9 @@ class ACLEDInterpolator:
 
         pes = self.photo_electrons.copy()
         params = []
+
+        cubic_spline = interp1d(self.ac_level, pes.T, kind='quadratic',
+                                bounds_error=False,)
 
         for i, pe in enumerate(pes.T):
 
@@ -67,7 +82,7 @@ class ACLEDInterpolator:
             params.append(param)
 
         params = np.array(params)
-        return params
+        return params, cubic_spline
 
     def plot(self, axes=None, pixel=0, **kwargs):
 