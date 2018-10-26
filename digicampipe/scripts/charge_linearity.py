a44edae2133ea6bcab6b9fdd4e9a14877eab7431
Cyril Alispach <cyril.alispach@gmail.com>
2018-10-26 09:48:05 +0200

    - table interpolator for negative charges reflected with positive values f(x) = - f(-x) - save baseline - split charge linearity and charge resolution

--- digicampipe/scripts/charge_linearity.py
+++ digicampipe/scripts/charge_linearity.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from digicampipe.utils.led import ACLEDInterpolator
-from histogram.histogram import Histogram1D
+from digicampipe.instrument.light_source import ACLED
 from scipy.interpolate import interp1d
 from tqdm import tqdm
+import os
 
 from digicampipe.calib.baseline import subtract_baseline, fill_digicam_baseline
 from digicampipe.calib.charge import \
-    compute_charge_with_saturation_and_threshold
+    compute_charge_with_saturation_and_threshold, compute_number_of_pe_from_table
 from digicampipe.io.event_stream import calibration_event_stream
 from digicampipe.visualization.plot import plot_histo, plot_array_camera
 
@@ -25,70 +25,98 @@ def compute_linearity(measured_pe, true_pe):
     return linearity_func
 
 
-data = np.load('charge_linearity_calib.npz')
-measured_charge = data['charge_mean']
-ac_leds = np.load('/home/alispach/data/tests/mpe/mpe_fit_results.npz')
-ac_levels = ac_leds['ac_levels']
-pe = ac_leds['mu']
-pe_err = ac_leds['mu_error']
+def charge_to_pe(x, measured_average_charge, true_pe):
 
-ac_led = ACLEDInterpolator(ac_levels, pe, pe_err)
+    X = measured_average_charge.T
+    Y = true_pe.T
 
+    dX = np.diff(X, axis=-1)
+    dY = np.diff(Y, axis=-1)
+
+    sign = np.sign(x)
+
+    w = np.clip((np.abs(x[:, None]) - X[:, :-1]) / dX[:, :], 0, 1)
+
+    y = Y[:, 0] + np.nansum(w * dY[:, :], axis=1)
+    y = y * sign
+    return y
+
+
+integral_width = 7
+# saturation_threshold = dict(np.load('/home/alispach/Documents/PhD/ctasoft/digicampipe/thresholds.npz'))
+# saturation_threshold = saturation_threshold['threshold_charge']
+
+# mean = np.nanmean(saturation_threshold)
+# saturation_threshold[np.isnan(saturation_threshold)] = mean
+
+saturation_threshold = 3000
+
+max_events = None
+directory = '/sst1m/analyzed/calib/mpe/'
+file_calib = os.path.join(directory, 'mpe_fit_results_combined.npz')
+data_calib = np.load(file_calib)
+
+ac_levels = data_calib['ac_levels'][:, 0]
+pe = data_calib['mu']
+pe_err = data_calib['mu_error']
+ac_led = ACLED(ac_levels, pe, pe_err)
 true_pe = ac_led(ac_levels).T
+# mask = true_pe < 5
+# true_pe[mask] = pe[mask]
 
-print(measured_charge.shape, true_pe.shape)
+
 files = ['/sst1m/raw/2018/06/28/SST1M_01/SST1M_01_20180628_{}.fits.fz' \
          ''.format(i) for i in range(1350, 1454 + 1, 1)]
-# files = files[80:]
-ac_levels = np.hstack([np.arange(0, 20, 1), np.arange(20, 450, 5)])
+# files = files[100:]
+# ac_levels = ac_levels[100:]
 n_pixels = 1296
 n_files = len(files)
-filename = 'charge_linearity_final_final_final_final_final.npz'
+
+assert n_files == len(ac_levels)
+filename_1 = 'charge_linearity_24102018.npz'
+filename_2 = 'charge_resolution_24102018.npz'
+
 debug = False
+pulse_tail = False
 shape = (n_files, n_pixels)
 amplitude_mean = np.zeros(shape)
 amplitude_std = np.zeros(shape)
+baseline_mean = np.zeros(shape)
+baseline_std = np.zeros(shape)
 charge_mean = np.zeros(shape)
 charge_std = np.zeros(shape)
 pe_mean = np.zeros(shape)
 pe_std = np.zeros(shape)
 
-
-timing_histo = Histogram1D.load('/home/alispach/data/tests/'
-                                'timing/timing_histo.pk')
-timing_histo.draw(index=100)
-timing = timing_histo.mode() // 4
+timing = np.load('/sst1m/analyzed/calib/timing/timing.npz')
+timing = timing['time'] // 4
 plot_array_camera(timing)
 plot_histo(timing)
 
-plt.figure()
-plt.scatter(true_pe, measured_charge, s=1)
-plt.show()
-
 for i, file in tqdm(enumerate(files), total=n_files):
 
-    events = calibration_event_stream(file, max_events=100)
+    events = calibration_event_stream(file, max_events=max_events)
     events = fill_digicam_baseline(events)
     events = subtract_baseline(events)
     # events = compute_charge_with_saturation(events, integral_width=7)
     events = compute_charge_with_saturation_and_threshold(events,
-                                                          integral_width=7,
+                                                          integral_width=integral_width,
                                                           debug=debug,
-                                                          measured_charge=measured_charge,
-                                                          true_charge=true_pe,
                                                           trigger_bin=timing,
-                                                          saturation_threshold=3000)
+                                                          saturation_threshold=saturation_threshold,
+                                                          pulse_tail=pulse_tail)
     # events = compute_maximal_charge(events)
 
     for n, event in enumerate(events):
 
         charge_mean[i] += event.data.reconstructed_charge
         amplitude_mean[i] += event.data.reconstructed_amplitude
-        pe_mean[i] += event.data.reconstructed_number_of_pe
 
         charge_std[i] += event.data.reconstructed_charge**2
         amplitude_std[i] += event.data.reconstructed_amplitude**2
-        pe_std[i] += event.data.reconstructed_number_of_pe**2
+
+        baseline_mean[i] += event.data.baseline
+        baseline_std[i] += event.data.baseline**2
 
     charge_mean[i] = charge_mean[i] / (n + 1)
     charge_std[i] = charge_std[i] / (n + 1)
@@ -96,32 +124,43 @@ for i, file in tqdm(enumerate(files), total=n_files):
     amplitude_mean[i] = amplitude_mean[i] / (n + 1)
     amplitude_std[i] = amplitude_std[i] / (n + 1)
     amplitude_std[i] = np.sqrt(amplitude_std[i] - amplitude_mean[i]**2)
+    baseline_mean[i] = baseline_mean[i] / (n + 1)
+    baseline_std[i] = baseline_std[i] / (n + 1)
+    baseline_std[i] = np.sqrt(baseline_std[i] - baseline_mean[i]**2)
+
+np.savez(filename_1, charge_mean=charge_mean, charge_std=charge_std,
+         amplitude_mean=amplitude_mean, amplitude_std=amplitude_std,
+         ac_levels=ac_levels, pe=pe, pe_err=pe_err, baseline_mean=baseline_mean,
+         baseline_std=baseline_std)
+
+charge_mean = np.load(filename_1)['charge_mean']
+pe_interpolator = lambda x: charge_to_pe(x, charge_mean, true_pe)
+
+for i, file in tqdm(enumerate(files), total=n_files):
+
+    events = calibration_event_stream(file, max_events=max_events)
+    events = fill_digicam_baseline(events)
+    events = subtract_baseline(events)
+    # events = compute_charge_with_saturation(events, integral_width=7)
+    events = compute_charge_with_saturation_and_threshold(events,
+                                                          integral_width=integral_width,
+                                                          debug=debug,
+                                                          trigger_bin=timing,
+                                                          saturation_threshold=saturation_threshold,
+                                                          pulse_tail=pulse_tail)
+
+    events = compute_number_of_pe_from_table(events, pe_interpolator)
+    # events = compute_maximal_charge(events)
+
+    for n, event in enumerate(events):
+
+        pe_mean[i] += event.data.reconstructed_number_of_pe
+        pe_std[i] += event.data.reconstructed_number_of_pe**2
 
     pe_mean[i] = pe_mean[i] / (n + 1)
     pe_std[i] = pe_std[i] / (n + 1)
     pe_std[i] = np.sqrt(pe_std[i] - pe_mean[i]**2)
 
-np.savez(filename, charge_mean=charge_mean, charge_std=charge_std,
-         amplitude_mean=amplitude_mean, amplitude_std=amplitude_std,
-         ac_levels=ac_levels, pe_mean=pe_mean, pe_std=pe_std)
+np.savez(filename_2, pe_reco_mean=pe_mean, pe_reco_std=pe_std,
+         ac_levels=ac_levels, pe=pe, pe_err=pe_err, true_pe=true_pe)
 
-plt.figure()
-plt.scatter(charge_mean.ravel(), amplitude_mean.ravel())
-
-
-x = np.arange(1, charge_mean.max().astype(int))
-plt.figure()
-plt.scatter(charge_mean.ravel(), charge_std.ravel()/charge_mean.ravel())
-plt.scatter(amplitude_mean.ravel(),
-            amplitude_std.ravel()/amplitude_mean.ravel())
-plt.yscale('log')
-plt.xscale('log')
-plt.plot(x, 1/np.sqrt(x))
-
-plt.figure()
-plt.scatter(pe_mean, pe_std.ravel()/pe_mean.ravel(), s=1, color='k')
-plt.yscale('log')
-plt.xscale('log')
-plt.plot(x, 1/np.sqrt(x))
-plt.xlim(1, 10000)
-plt.show()
