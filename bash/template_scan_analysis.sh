fd685136020fa72de4e2c355af7e75dcaa2f3295
Yves Renier <yves.renier@unige.ch>
2018-11-01 10:59:07 +0100

    add bash script to create the template

--- bash/template_scan_analysis.sh
+++ bash/template_scan_analysis.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-DATA_BASE_DIR=/home/dneise/sst_new/pdp
+output=/sst1m/analyzed/template_scan/20180522
+digicam_path=~/ctasoft/digicampipe
+DATA_BASE_DIR=/sst1m/raw/2018/
 
-digicam-template \
-    --output=template_scan_dac_250.h5 \
-    $DATA_BASE_DIR/2018/05/22/SST1M_01_20180522_{010..041}.fits.fz &
+# create a 2D histogram per pixel from
+python $digicam_path/digicam/scripts/pulse_shape.py --output=$output/template_scan_dac_250.fits.gz $DATA_BASE_DIR/05/22/SST1M_01_20180522_{010..041}.fits.fz
+python $digicam_path/digicam/scripts/pulse_shape.py --output=$output/template_scan_dac_400.fits.gz $DATA_BASE_DIR/05/15/SST1M_01_20180515_{394..425}.fits.fz
+python $digicam_path/digicam/scripts/pulse_shape.py --output=$output/template_scan_dac_450.fits.gz $DATA_BASE_DIR/05/22/SST1M_01_20180522_{046..075}.fits.fz
 
-digicam-template \
-    --output=template_scan_dac_400.h5 \
-    $DATA_BASE_DIR/2018/05/15/SST1M_01_20180515_{394..425}.fits.fz &
+# create templates from the histograms
 
-digicam-template \
-    --output=template_scan_dac_450.h5 \
-    $DATA_BASE_DIR/2018/05/22/SST1M_01_20180522_{046..075}.fits.fz &
+python $digicam_path/digicam/scripts/pulse_template.py --output=$output/pulse_template_all_pixels.txt --plot=$output/pulse_template_all_pixels.png $output/template_scan_dac_*0.fits.gz
