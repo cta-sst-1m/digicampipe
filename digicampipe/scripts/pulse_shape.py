061ce397417f5a8ee19fd0fafba1b40d1f3b602e
Yves Renier <yves.renier@unige.ch>
2018-11-02 14:24:11 +0100

    add possiblity to specify delays for each file for pulse_shape.py script

--- digicampipe/scripts/pulse_shape.py
+++ digicampipe/scripts/pulse_shape.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 """
-Create an histogram of the pulse template for each pixel.
+Create an histogram of the normalise pulse shape for each pixel.
 Usage:
   digicam-pulse-shape [options] <input_files>...
 
 Options:
   -h --help                 Show this screen.
+  <input_files>             Path to zfits input files.
+  --delays_ps=LIST          Delays used [in ps] for each input file. If set to
+                            "none", the timing of each pulse will be
+                            determined at the half height of the rising
+                            edge. If different of none, the coma separated list
+                            must have as many values as inputs files.
+                            [default: none]
   --output_hist=PATH        Output histogram file, if not given, we just append
                             ".npz" to the path of the 1st input file.
   --time_range_ns=LIST      Minimum and maximum time in ns w.r.t. half maximum
@@ -35,8 +42,9 @@ from digicampipe.utils.docopt import convert_list_float, convert_list_int, \
 
 
 def main(
-        output_hist,
         input_files,
+        output_hist,
+        delays_ps=None,
         time_range_ns=(-10., 40.),
         amplitude_range=(-0.1, 0.4),
         integration_range=(8, 20),
@@ -46,45 +54,53 @@ def main(
         n_bin=100,
         disable_bar=False
 ):
-    if os.path.exists(output_hist):
-        os.remove(output_hist)
+    if delays_ps is not None:
+        assert len(delays_ps) == len(input_files)
     charge_min = np.min(charge_range)
     charge_max = np.max(charge_range)
     integration_min = np.min(integration_range)
     integration_max = np.max(integration_range)
-    events = calibration_event_stream(input_files, disable_bar=disable_bar)
-    events = fill_digicam_baseline(events)
-    if "SST1M_01_201805" in input_files[0]:  # fix data in May
-        print("WARNING: correction of the baselines applied.")
-        events = correct_wrong_baseline(events)
-    events = subtract_baseline(events)
     histo = None
     n_sample = 0
     n_pixel = 0
-    for e in events:
-        adc = e.data.adc_samples
-        integral = adc[:, slice(integration_min, integration_max)].sum(axis=1)
-        adc_norm = adc / integral[:, None]
-        arrival_time_in_ns = estimate_time_from_leading_edge(adc) * 4
-        if histo is None:
-            n_pixel, n_sample = adc_norm.shape
-            histo = Histogram2dChunked(
-                shape=(n_pixel, n_bin, n_bin),
-                range=[time_range_ns, amplitude_range]
+    for file_idx, input_file in enumerate(input_files):
+        if not os.path.isfile(input_file):
+            continue
+        events = calibration_event_stream([input_file], disable_bar=disable_bar)
+        events = fill_digicam_baseline(events)
+        if "SST1M_01_201805" in input_files[0]:  # fix data in May
+            print("WARNING: correction of the baselines applied.")
+            events = correct_wrong_baseline(events)
+        events = subtract_baseline(events)
+        for e in events:
+            adc = e.data.adc_samples
+            integral = adc[:, slice(integration_min, integration_max)].sum(axis=1)
+            adc_norm = adc / integral[:, None]
+            if delays_ps is None:
+                arrival_time_in_ns = estimate_time_from_leading_edge(adc) * 4
+            else:
+                arrival_time_in_ns = 1e-3 * delays_ps[file_idx]
+            if histo is None:
+                n_pixel, n_sample = adc_norm.shape
+                histo = Histogram2dChunked(
+                    shape=(n_pixel, n_bin, n_bin),
+                    range=[time_range_ns, amplitude_range]
+                )
+            else:
+                assert adc_norm.shape[0] == n_pixel
+                assert adc_norm.shape[1] == n_sample
+            time_in_ns = np.arange(n_sample) * 4
+            bad_charge = np.logical_or(
+                integral < charge_min,
+                integral > charge_max
             )
-        else:
-            assert adc_norm.shape[0] == n_pixel
-            assert adc_norm.shape[1] == n_sample
-        time_in_ns = np.arange(n_sample) * 4
-        bad_charge = np.logical_or(
-            integral < charge_min,
-            integral > charge_max
-        )
-        arrival_time_in_ns[bad_charge] = -np.inf  # ignored by histo
-        histo.fill(
-            x=time_in_ns[None, :] - arrival_time_in_ns[:, None],
-            y=adc_norm
-        )
+            arrival_time_in_ns[bad_charge] = -np.inf  # ignored by histo
+            histo.fill(
+                x=time_in_ns[None, :] - arrival_time_in_ns[:, None],
+                y=adc_norm
+            )
+    if os.path.exists(output_hist):
+        os.remove(output_hist)
     histo.save(output_hist)
     print('2D histogram of pulse shape for all pixel saved as', output_hist)
 
@@ -93,6 +109,7 @@ def entry():
     args = docopt(__doc__)
     inputs = args['<input_files>']
     output_hist = args['--output_hist']
+    delays = convert_list_float(args['--delays_ps'])
     time_range_ns = convert_list_float(args['--time_range_ns'])
     amplitude_range = convert_list_float(args['--amplitude_range'])
     integration_range = convert_list_int(args['--integration_range'])
@@ -111,8 +128,9 @@ def entry():
     print('charge_range:', charge_range)
     print('n_bin:', n_bin)
     main(
-        output_hist=output_hist,
         input_files=inputs,
+        output_hist=output_hist,
+        delays_ps=delays_ps,
         time_range_ns=time_range_ns,
         amplitude_range=amplitude_range,
         integration_range=integration_range,
