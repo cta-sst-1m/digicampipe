4b714db1ffbe950e2b66f221481aeb616a175135
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-07 11:01:49 +0100

    testing and improvement for LightSource

--- digicampipe/instrument/light_source.py
+++ digicampipe/instrument/light_source.py
@@ -23,9 +23,13 @@ class LightSource(ABC):
         self.y_err = y_err
         self._interpolator = self._interpolate()
 
-    def __call__(self, x, *args, **kwargs):
+    def __call__(self, x, pixel=None, *args, **kwargs):
 
         y_interpolated = self._interpolator(x, *args, **kwargs)
+
+        if pixel is not None:
+
+            y_interpolated = y_interpolated[pixel]
 
         return y_interpolated
 
@@ -34,16 +38,13 @@ class LightSource(ABC):
         return self.__class__(x=self.x, y=self.y[item],
                               y_err=self.y_err[item])
 
-    @classmethod
-    @abstractmethod
-    def load(cls, filename):
-
-        pass
-
-    @abstractmethod
     def save(self, filename):
 
-        pass
+        with fitsio.FITS(filename, 'rw', clobber=True) as f:
+
+            f.write(self.x, extname='x')
+            f.write(self.y, extname='y', compress='gzip')
+            f.write(self.y_err, extname='y_err', compress='gzip')
 
     def plot(self, axes=None, pixel=0, **kwargs):
 
@@ -80,16 +81,31 @@ class LightSource(ABC):
 
 class ACLED(LightSource):
 
-    def __init__(self, ac_level, photo_electrons, photo_electrons_err=None,
-                 saturation_threshold=300):
+    saturation_threshold = 300  # in p.e.
 
-        self.ac_level = ac_level - ac_level[0]
-        self.photo_electrons = photo_electrons
-        self.photo_electrons_err = photo_electrons_err
-        self.saturation_threshold = saturation_threshold  # in p.e.
+    def __init__(self, ac_level, photo_electrons, photo_electrons_err=None):
+
+        assert ac_level[0] == 0
+        assert (np.diff(ac_level) > 0).all()
+
+        self.x = ac_level - ac_level[0]
+        self.y = photo_electrons
+        self.y_err = photo_electrons_err
         self._interpolate()
         self._extrapolate()
         self._extrapolate_exponential()
+
+    @classmethod
+    def load(cls, filename):
+
+        with fitsio.FITS(filename, 'r') as f:
+            x = f['x'].read()
+            y = f['y'].read()
+            y_err = f['y_err'].read()
+
+        obj = ACLED(x, y, y_err)
+
+        return obj
 
     def __call__(self, ac_level, pixel=None):
 
@@ -110,35 +126,22 @@ class ACLED(LightSource):
 
             y = y[pixel]
 
-        # mask = np.isfinite(y)
-        # assert (np.diff(y[mask]) > 0).all()
-
         return y
-
-    def __getitem__(self, item):
-
-        return ACLED(ac_level=self.ac_level,
-                     photo_electrons=self.photo_electrons[item])
-
-    @classmethod
-    def load(cls, filename):
-
-        raise NotImplementedError
 
     def _extrapolate(self):
 
-        pes = self.photo_electrons.copy()
+        pes = self.y.copy()
         params = []
         deg = 4
 
         for i, pe in enumerate(pes.T):
 
-            ac_level = self.ac_level.copy()
+            ac_level = self.x.copy()
             mask = (pe > 50) * (pe < self.saturation_threshold) * np.isfinite(pe)
 
             err = None
-            if self.photo_electrons_err is not None:
-                err = self.photo_electrons_err[:, i]
+            if self.y_err is not None:
+                err = self.y_err[:, i]
                 mask = mask * (np.isfinite(err))
                 err = err[mask]
                 err = 1 / err
@@ -165,16 +168,16 @@ class ACLED(LightSource):
 
     def _extrapolate_exponential(self):
 
-        pes = self.photo_electrons.copy()
+        pes = self.y.copy()
         params = []
 
         for i, pe in enumerate(pes.T):
 
-            ac_level = self.ac_level.copy()
+            ac_level = self.x.copy()
             mask = (pe > 0) * (pe < 100) * np.isfinite(pe) * (ac_level >= 0)
 
-            if self.photo_electrons_err is not None:
-                err = self.photo_electrons_err[:, i]
+            if self.y_err is not None:
+                err = self.y_err[:, i]
                 mask = mask * (np.isfinite(err))
                 err = err[mask]
 
@@ -223,7 +226,7 @@ class ACLED(LightSource):
 
     def func_exponential(self, x):
 
-        y = np.zeros((self.photo_electrons.shape[1], len(x)))
+        y = np.zeros((self.y.shape[1], len(x)))
 
         for i in range(len(y)):
             y[i] = exponential(x, self._params_exponential[i][0],
@@ -233,7 +236,7 @@ class ACLED(LightSource):
 
     def func_spline(self, x):
 
-        y = np.zeros((self.photo_electrons.shape[1], len(x)))
+        y = np.zeros((self.y.shape[1], len(x)))
 
         for i in range(len(y)):
             y[i] = self._spline[i](x)
@@ -242,7 +245,7 @@ class ACLED(LightSource):
 
     def func_polynomial(self, x):
 
-        y = np.zeros((self.photo_electrons.shape[1], len(x)))
+        y = np.zeros((self.y.shape[1], len(x)))
 
         for i in range(len(y)):
             y[i] = np.polyval(self._params[i], x).T
@@ -251,18 +254,18 @@ class ACLED(LightSource):
 
     def _interpolate(self):
 
-        pes = self.photo_electrons.copy().T
+        pes = self.y.copy().T
         cubic_spline = []
 
         for pe in pes:
 
             mask = np.isfinite(pe) # * (pe > 5)
-            x = self.ac_level[mask]
+            x = self.x[mask]
             y = pe[mask]
 
             if not len(x):
 
-                x = self.ac_level
+                x = self.x
                 y = pe
 
             spline = interp1d(x, y,
@@ -289,10 +292,6 @@ class ACLED(LightSource):
 
         self._spline = cubic_spline
 
-    def save(self, filename):
-
-        pass
-
     def plot(self, axes=None, pixel=0, y_lim=(0, 2000), show_fits=True, **kwargs):
 
         if axes is None:
@@ -306,14 +305,14 @@ class ACLED(LightSource):
         x_fit = x_fit[mask]
         y_fit = y_fit[mask]
 
-        if self.photo_electrons_err is not None:
+        if self.y_err is not None:
 
-            y_err = self.photo_electrons_err[:, pixel]
+            y_err = self.y_err[:, pixel]
         else:
 
             y_err = None
 
-        axes.errorbar(self.ac_level, self.photo_electrons[:, pixel],
+        axes.errorbar(self.x, self.y[:, pixel],
                       yerr=y_err, label='Data points, pixel : {}'.format(pixel),
                       linestyle='None', marker='o', color='k', **kwargs)
         axes.plot(x_fit, y_fit, label='Interpolated data', color='r')
@@ -367,8 +366,15 @@ if __name__ == '__main__':
     ac_leds = np.load('/sst1m/analyzed/calib/mpe/mpe_fit_results_combined.npz')
 
     ac_levels = ac_leds['ac_levels'][:, 0]
+
     pe = ac_leds['mu']
+
+
     pe_err = ac_leds['mu_error']
+    print(ac_levels.tolist())
+    print(pe[:, 0:2].tolist())
+    print(pe_err[:, 0:2].tolist())
+
 
     test = ACLED(ac_levels, pe, pe_err)
 
