d109ad63d6243be2b6512168744e08f417154901
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-08 15:40:43 +0100

    adding main.sh to create env. variables for job

---
+++ bash/main.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+export DIGICAM_FOLDER='/sst1m/analyzed/calib/'
+export DIGICAM_GHV_OFF_FILES=($(echo /sst1m/raw/2018/06/27/SST1M_01/SST1M_01_20180627_{1292..1299}.fits.fz))
+export DIGICAM_GHV_ON_FILES=($(echo /sst1m/raw/2018/06/27/SST1M_01/SST1M_01_20180627_{1286..1292}.fits.fz))
+export DIGICAM_DARK_FILES=($(echo sst1m/raw/2018/06/27/SST1M_01/SST1M_01_20180627_{1276..1279}.fits.fz))
+export DIGICAM_AC_FILES=($(echo /sst1m/raw/2018/06/28/SST1M_01/SST1M_01_20180628_{1350..1454}.fits.fz))
+export DIGICAM_DC_FILES=($(echo /sst1m/raw/2018/06/28/SST1M_01/SST1M_01_20180628_{1455..1504}.fits.fz))
+export DIGICAM_AC_DC_FILES_1=($(echo /sst1m/raw/2018/06/28/SST1M_01/SST1M_01_20180628_{1505..2087}.fits.fz))
+export DIGICAM_AC_DC_FILES_2=($(echo /sst1m/raw/2018/07/01/SST1M_01/SST1M_01_20180701_{434..497}.fits.fz))
+
+
+export DIGICAM_AC_LEVEL=({0..19..1} {20..35..5} {45..445..5})
+export DIGICAM_AC_LEVEL_1=({0..18..2} {20..440..10})
+export DIGICAM_AC_LEVEL_2=({0..18..2} {20..440..20})
+export DIGICAM_DC_LEVEL=({200..445..5})
+export DIGICAM_DC_LEVEL_1=({200..440..10})
+export DIGICAM_DC_LEVEL_2=(${DIGICAM_DC_LEVEL_1[@]:12:2})
+export DIGICAM_DC_LEVEL_1=(${DIGICAM_DC_LEVEL_1[@]:0:11})
+
+
+export DIGICAM_PIXELS=({0..1295..1})
+export DIGICAM_INTEGRAL_WIDTH=7
+export DIGICAM_INTEGRAL_SHIFT=0
+export DIGICAM_N_SAMPLES=50
+export DIGICAM_GAIN_APPROX=20
+export DIGICAM_LSB_MIN=-10
+export DIGICAM_LSB_MAX=3000
+
+
+# echo $DIGICAM_DC_SCAN_FILES
+# echo ${#DIGICAM_AC_LEVEL[@]}
+# echo ${#DIGICAM_DC_LEVEL[@]}
+# echo ${#DIGICAM_AC_FILES[@]}
+# echo ${#DIGICAM_DC_FILES[@]}
+# echo ${#DIGICAM_AC_DC_FILES_1[@]} ${DIGICAM_AC_LEVEL_1[@]} ${DIGICAM_DC_LEVEL_1[@]}
+# echo ${#DIGICAM_AC_DC_FILES_2[@]} ${#DIGICAM_AC_LEVEL_2[@]} ${#DIGICAM_DC_LEVEL_2[@]}
+
+# echo ${#DIGICAM_PIXELS[@]}
+# (IFS=,; echo "${DIGICAM_AC_LEVEL[*]}")
+
+# val echo /sst1m/raw/2018/06/28/SST1M_01/SST1M_01_20180628_{1455..1504}.fits.fz
