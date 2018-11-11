598cc31ab61c16b736b99de20825d5e388c6de12
Yves Renier <yves.renier@unige.ch>
2018-11-11 03:12:44 +0100

    use new ressources

--- digicampipe/tests/test_pipeline.py
+++ digicampipe/tests/test_pipeline.py
@@ -8,20 +8,36 @@ from digicampipe.scripts.pipeline import main as main_pipeline
 from digicampipe.scripts.raw import compute as compute_raw
 from digicampipe.utils.docopt import convert_pixel_args
 
-example_file1_path = resource_filename(
+dark100_file_path = resource_filename(
     'digicampipe',
     os.path.join(
         'tests',
         'resources',
-        'example_100_evts.000.fits.fz'
+        'dark_100_evts.fits.fz'
     )
 )
-example_file2_path = resource_filename(
+dark200_file_path = resource_filename(
     'digicampipe',
     os.path.join(
         'tests',
         'resources',
-        'example_10_evts.000.fits.fz'
+        'dark_200_evts.fits.fz'
+    )
+)
+science100_file_path = resource_filename(
+    'digicampipe',
+    os.path.join(
+        'tests',
+        'resources',
+        'run150_100_evts.fits.fz'
+    )
+)
+science200_file_path = resource_filename(
+    'digicampipe',
+    os.path.join(
+        'tests',
+        'resources',
+        'run150_200_evts.fits.fz'
     )
 )
 calibration_filename = resource_filename(
@@ -37,7 +53,7 @@ template_filename = resource_filename(
     os.path.join(
         'tests',
         'resources',
-        'pulse_SST-1M_pixel_0.dat'
+        'pulse_template_all_pixels.txt'
     )
 )
 expected_columns = ['phi', 'y', 'skewness', 'intensity', 'x', 'event_id',
@@ -51,13 +67,13 @@ def test_pipeline():
         dark_filename = os.path.join(tmpdirname, 'dark.pk')
         hillas_filename = os.path.join(tmpdirname, 'hillas.fits')
         compute_raw(
-            files=[example_file1_path],
+            files=[dark200_file_path],
             max_events=None,
             pixel_id=convert_pixel_args(None),
             filename=dark_filename
         )
         main_pipeline(
-            files=[example_file2_path],
+            files=[science200_file_path],
             max_events=None,
             dark_filename=dark_filename,
             shift=0,
@@ -68,8 +84,8 @@ def test_pipeline():
             parameters_filename=calibration_filename,
             compute=True,
             display=False,
-            picture_threshold=1,  # unusual value, so events pass cuts
-            boundary_threshold=1,  # unusual value, so events pass cuts
+            picture_threshold=30,
+            boundary_threshold=15,
         )
         hdul = fits.open(os.path.join(tmpdirname, 'hillas.fits'))
         cols = [c.name for c in hdul[1].columns]
