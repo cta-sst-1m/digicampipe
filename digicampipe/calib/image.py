608bd79a23282dacea82ef6fe57917e33be66a2f
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-16 15:14:17 +0100

    catch hillas error and use masked arrays

--- digicampipe/calib/image.py
+++ digicampipe/calib/image.py
@@ -1,12 +1,21 @@
-from ctapipe.image import hillas_parameters
+from ctapipe.image import hillas_parameters, HillasParameterizationError
+import numpy as np
 
 
 def compute_hillas_parameters(events, geom):
+
     for event in events:
         mask = event.data.cleaning_mask
         image = event.data.reconstructed_number_of_pe
-        image[~mask] = 0
-        hillas = hillas_parameters(geom, image)
-        event.hillas = hillas
+        image = np.ma.masked(image, mask=mask)
 
-        yield event
+        try:
+
+            hillas = hillas_parameters(geom, image)
+            event.hillas = hillas
+
+            yield event
+
+        except HillasParameterizationError:
+
+            continue
