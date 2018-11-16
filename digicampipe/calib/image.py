6868490cdadbf63f8a8975ee17e5908b3f0827f8
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-16 16:36:10 +0100

    space added

--- digicampipe/calib/image.py
+++ digicampipe/calib/image.py
@@ -5,6 +5,7 @@ import numpy as np
 def compute_hillas_parameters(events, geom):
 
     for event in events:
+
         mask = event.data.cleaning_mask
         image = event.data.reconstructed_number_of_pe
         image = np.ma.masked_array(image, mask=~mask)
