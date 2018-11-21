969a6db9dd86f0e3333377c00d980805434d50e4
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-21 10:54:52 +0100

    adding bash script for rate scan

---
+++ bash/rate_scan.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+FILE=$1
+START=0
+END=4095
+STEP=1
+N_SAMPLES=1024
+
+DIR=$(dirname "${FILE}")
+BASENAME=$(basename "${FILE}")
+BASENAME=$(echo $BASENAME | cut -d'.' -f 1)
+FIGURE=$DIR'/rate_scan_'$BASENAME'.png'
+OUTPUT=$DIR'/rate_scan_'$BASENAME'.fits'
+
+digicam-rate-scan --compute --threshold_step=$STEP --threshold_end=$END --threshold_start=$START --n_samples=$N_SAMPLES --figure_path=$FIGURE --output=$OUTPUT $FILE
