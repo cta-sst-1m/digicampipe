b2281553e600d5253d4f449bb69d249de67f6037
Cyril Alispach <cyril.alispach@gmail.com>
2018-11-21 12:07:13 +0100

    write output according to filename

--- bash/rate_scan.sh
+++ bash/rate_scan.sh
@@ -7,9 +7,13 @@ STEP=1
 N_SAMPLES=1024
 
 DIR=$(dirname "${FILE}")
+DIR=$(echo $DIR | cut -d '/' -f  5,6,7)
+DIR='/sst1m/analyzed/'$DIR
+ls $DIR -ltr
+
 BASENAME=$(basename "${FILE}")
 BASENAME=$(echo $BASENAME | cut -d'.' -f 1)
 FIGURE=$DIR'/rate_scan_'$BASENAME'.png'
 OUTPUT=$DIR'/rate_scan_'$BASENAME'.fits'
 
-digicam-rate-scan --compute --threshold_step=$STEP --threshold_end=$END --threshold_start=$START --n_samples=$N_SAMPLES --figure_path=$FIGURE --output=$OUTPUT $FILE
+echo digicam-rate-scan --compute --threshold_step=$STEP --threshold_end=$END --threshold_start=$START --n_samples=$N_SAMPLES --figure_path=$FIGURE --output=$OUTPUT $FILE
