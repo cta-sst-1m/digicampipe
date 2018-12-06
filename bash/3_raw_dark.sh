3da15375893bf8f1d76227e4aa0f3fe519aecab5
Cyril Alispach <cyril.alispach@gmail.com>
2018-12-06 11:04:00 +0100

    ...to pdf

--- bash/3_raw_dark.sh
+++ bash/3_raw_dark.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#SBATCH --time=02:00:00
+#SBATCH --time=06:00:00
 #SBATCH --partition=mono
 #SBATCH --mem=4G
 #SBATCH --output=/home/%u/job_logs/%x-%A_%a.out
@@ -9,6 +9,6 @@ source 0_main.sh
 
 pixels=$(echo ${DIGICAM_PIXELS[@]} | tr -s ' ' ',')
 
-# digicam-raw --compute --output=$DARK_RAW_HISTO --pixel=$pixels ${DIGICAM_DARK_FILES[@]} --figure_path=$DIGICAM_FOLDER'figures/raw_histo.pdf'
+# digicam-raw --compute --output=$DARK_RAW_HISTO --pixel=$pixels ${DIGICAM_DARK_FILES[@]}
 # digicam-rate-scan --compute --output=$DARK_RATE_SCAN --threshold_step=10 --threshold_end=100 ${DIGICAM_DARK_FILES[@]}
 digicam-raw --figure_path=$DIGICAM_FOLDER'figures/raw_histo.pdf' --output=$DARK_RAW_HISTO ${DIGICAM_DARK_FILES[@]}
