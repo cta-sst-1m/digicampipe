Normalization didn't work correctly in an array of templates. Instead of looking for the maximum in each template, in look for the maximum in among all the templates We fix this error by applying the restriction for each template. hen we create a test to check it didn't break previous code.

digicampipe/tests/test_pulse_template_object.py
@@ -181,5 +181,17 @@ def test_save_and_load_fits():
                       template_loaded.amplitude_std)
 
 
+def test_normalization_for_array_template():
+
+    data_1 = np.array([[1, 1, 1, 1, 1, 1, 1, 1, 9, 2, 5, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 13, -4, 5, 1, 1, 1, 1, 1, 1]])
+    data_2 = -np.array([[1, 1, 1, 1, 1, 1, 1, 1, 9, 2, 5, 1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1, 1, 1, 13, -4, 5, 1, 1, 1, 1, 1, 1]])
+    time = np.arange(data_1.shape[1])
+    template_positive = NormalizedPulseTemplate(amplitude=data_1, time=time)
+    template_negative = NormalizedPulseTemplate(amplitude=data_2, time=time)
+
+    assert (template_positive.amplitude.max(axis=-1) == 1).all()
+    assert (template_negative.amplitude.max(axis=-1) == 1).all()
+
+
 if __name__ == '__main__':
-    test_pulse_template_save()
+    test_normalization_for_array_template()

digicampipe/utils/pulse_template.py
@@ -125,13 +125,13 @@ class NormalizedPulseTemplate:
         return cls(time=ts[0], amplitude=ampl[0], amplitude_std=ampl_std[0])
 
     def _interpolate(self):
-        if abs(np.min(self.amplitude)) <= abs(np.max(self.amplitude)):
 
-            normalization = np.max(self.amplitude)
+        min_amplitude = np.min(self.amplitude, axis=-1)
+        max_amplitude = np.max(self.amplitude, axis=-1)
 
-        else:
-
-            normalization = np.min(self.amplitude)
+        is_positive = np.abs(min_amplitude) <= np.abs(max_amplitude)
+        normalization = max_amplitude * is_positive + min_amplitude * ~is_positive
+        normalization = normalization[..., None]
 
         self.amplitude = self.amplitude / normalization
         self.amplitude_std = self.amplitude_std / normalization